5fa82b4b02974ac4ebd159721b2afdfb19477137
Cherik <Pourcheriki@gmail.com>
2024-08-07 19:01:26 +0330

    handle font size responsive

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -13,12 +13,16 @@ export const Banner = () => {
         fill
       />
       <h1 className="uppercase text-white text-center text-nowrap relative font-tusker-grotesk">
-        <div className="text-9xl tracking-[-4px]">the future of</div>
-        <div className="text-9xl font-black text-peach tracking-[-2px]">
+        <div className="text-4xl sm:text-7xl md:text-8xl lg:text-9xl xl:tracking-[-4px]">
+          the future of
+        </div>
+        <div className="text-4xl sm:text-7xl md:text-8xl lg:text-9xl xl:tracking-[-2px] font-black text-peach ">
           tokenization
         </div>
       </h1>
-      <div className={`text-white text-4xl mt-4 ${plex.className}`}>
+      <div
+        className={`text-white text-lg sm:text-xl lg:text-3xl xl:text-4xl mt-4 ${plex.className}`}
+      >
         q/acc = QF*ABC
       </div>
     </div>
