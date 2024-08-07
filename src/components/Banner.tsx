f094ae848265aa5af45670844147211e9881cdad
Cherik <Pourcheriki@gmail.com>
2024-08-07 18:09:57 +0330

    fix image

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,15 +7,15 @@ const grotesk = Hanken_Grotesk({ subsets: ["latin"] });
 export const Banner = () => {
   return (
     <div
-      className={`bg-particle-pattern py-16 flex flex-col justify-center items-center ${grotesk.className}`}
+      className={`relative bg-particle-pattern py-32 flex flex-col justify-center items-center ${grotesk.className}`}
     >
       <Image
+        className="p-16 object-contain"
         src="/images/home/banner-vector.svg"
         alt="Banner Vector"
-        height={447}
-        width={895}
+        fill
       />
-      <h1 className="uppercase text-white text-center text-nowrap ">
+      <h1 className="uppercase text-white text-center text-nowrap relative ">
         <div className="text-9xl tracking-[-9px]">the future of</div>
         <div className="text-9xl font-black text-peach tracking-[-2px]">
           tokenization
