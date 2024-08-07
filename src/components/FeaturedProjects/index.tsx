78d885c9298bade51587b6bcd27f10509f838371
Cherik <Pourcheriki@gmail.com>
2024-08-07 14:58:30 +0330

    add style od navigation

--- src/components/FeaturedProjects/index.tsx
+++ src/components/FeaturedProjects/index.tsx
@@ -3,6 +3,12 @@ import { ProjectCard } from "../ProjectCard/ProjectCard";
 import { Swiper, SwiperSlide, type SwiperClass } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
+import { IconPointerLeft } from "../Icons/IconPointerLeft";
+import { IconPointerRight } from "../Icons/IconPointerRight";
+
+const swiperSlideStyle = "!w-auto px-2 py-2";
+const navigationStyle =
+  "cursor-pointer rounded-full shadow-lg px-3 py-2 h-10 w-12 mx-2 select-none";
 
 export const FeaturedProjects = () => {
   const pagElRef = useRef<HTMLDivElement>(null);
@@ -12,20 +18,20 @@ export const FeaturedProjects = () => {
   const [_, setSwiperInstance] = useState<SwiperClass>();
 
   return (
-    <div className="bg-white mt-4">
-      <div className="container flex justify-between">
+    <div className="bg-white mt-4 py-10">
+      <div className="container flex justify-between items-center">
         <h2>#QuadraticAccelerator</h2>
         <div className="flex">
-          <div ref={prevElRef} className="prev-button">
-            Prev
+          <div ref={prevElRef} className={navigationStyle}>
+            <IconPointerLeft size={24} />
           </div>
-          <div ref={pagElRef} className="pagination"></div>
-          <div ref={nextElRef} className="next-button">
-            Next
+          <div ref={pagElRef} className="flex gap-1 items-center"></div>
+          <div ref={nextElRef} className={navigationStyle}>
+            <IconPointerRight size={24} />
           </div>
         </div>
       </div>
-      <div className="one-side-container bg-red-400 ">
+      <div className="one-side-container">
         <Swiper
           onSwiper={setSwiperInstance}
           modules={[Navigation, Pagination]}
@@ -46,34 +52,34 @@ export const FeaturedProjects = () => {
           slidesPerView={"auto"}
           spaceBetween={30}
         >
-          <SwiperSlide className="!w-auto">
+          <SwiperSlide className={swiperSlideStyle}>
             <ProjectCard />
           </SwiperSlide>
-          <SwiperSlide className="!w-auto">
+          <SwiperSlide className={swiperSlideStyle}>
             <ProjectCard />
           </SwiperSlide>
-          <SwiperSlide className="!w-auto">
+          <SwiperSlide className={swiperSlideStyle}>
             <ProjectCard />
           </SwiperSlide>
-          <SwiperSlide className="!w-auto">
+          <SwiperSlide className={swiperSlideStyle}>
             <ProjectCard />
           </SwiperSlide>
-          <SwiperSlide className="!w-auto">
+          <SwiperSlide className={swiperSlideStyle}>
             <ProjectCard />
           </SwiperSlide>
-          <SwiperSlide className="!w-auto">
+          <SwiperSlide className={swiperSlideStyle}>
             <ProjectCard />
           </SwiperSlide>
-          <SwiperSlide className="!w-auto">
+          <SwiperSlide className={swiperSlideStyle}>
             <ProjectCard />
           </SwiperSlide>
-          <SwiperSlide className="!w-auto">
+          <SwiperSlide className={swiperSlideStyle}>
             <ProjectCard />
           </SwiperSlide>
-          <SwiperSlide className="!w-auto">
+          <SwiperSlide className={swiperSlideStyle}>
             <ProjectCard />
           </SwiperSlide>
-          <SwiperSlide className="!w-auto">
+          <SwiperSlide className={swiperSlideStyle}>
             <ProjectCard />
           </SwiperSlide>
         </Swiper>
