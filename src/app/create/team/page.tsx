a5a8f435aa5099b19f952a85611d6348bbe87a51
Cherik <Pourcheriki@gmail.com>
2024-08-12 15:29:31 +0330

    init  AddTeamPage

---
+++ src/app/create/team/page.tsx
@@ -0,0 +1,10 @@
+import CreateTeamForm from "@/components/Create/CreateTeamForm";
+import React from "react";
+
+export default function AddTeamPage() {
+  return (
+    <div>
+      <CreateTeamForm />
+    </div>
+  );
+}
