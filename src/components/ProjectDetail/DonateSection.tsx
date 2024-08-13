46fda2e836027018fa0217e901f6c3d4785f1ef4
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2024-08-13 12:49:18 -0400

    added project detail page with its banner and tabs

---
+++ src/components/ProjectDetail/DonateSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DonateSection = () => {
+  return (
+    <div className=" lg:w-1/4  bg-white h-[450px] rounded-2xl p-6">
+      DonateSection
+    </div>
+  );
+};
+
+export default DonateSection;
