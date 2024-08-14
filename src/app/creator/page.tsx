29623d3fa667fb75845d2050a73556043df99a35
Cherik <Pourcheriki@gmail.com>
2024-08-15 00:25:44 +0330

    add creator page content of info section

--- src/app/creator/page.tsx
+++ src/app/creator/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import { Banner } from "@/components/Banner";
+import { Button, ButtonColor, ButtonStyle } from "@/components/Button";
 import Collaborator from "@/components/Collaborator";
 import InfoSection from "@/components/InfoSection";
 import React from "react";
@@ -12,7 +13,25 @@ export default function CreatorPage() {
         title2="Season one"
         subTitle="q/acc = QF*ABC"
       />
-      <InfoSection />
+      <InfoSection title="Welcome to season one!">
+        <p>
+          Quadratic Acceleration (q/acc) combines the strengths of Quadratic
+          Funding (QF) with those of Augmented Bonding Curves (ABC) in order to
+          create a new mechanism for launching tokens with built-in liquidity, a
+          passive revenue stream and a clear path to community growth.
+        </p>
+        <p>
+          The Quadratic Accelerator is a collaborative entity under Giveth
+          building the q/acc protocol based on the research of Commons Stack.
+        </p>
+        <Button
+          className="mx-auto !py-6 !px-10"
+          color={ButtonColor.Pink}
+          styleType={ButtonStyle.Solid}
+        >
+          Create Project
+        </Button>
+      </InfoSection>
       <About />
       <Collaborator />
     </main>
