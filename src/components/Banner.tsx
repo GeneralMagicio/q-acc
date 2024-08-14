e722d8591b455fac601ac650408d6d4591675372
Cherik <Pourcheriki@gmail.com>
2024-08-15 00:09:37 +0330

    change banner to accept content

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,16 @@
 import { IBM_Plex_Mono } from "next/font/google";
 import Image from "next/image";
+import { FC } from "react";
 
 const plex = IBM_Plex_Mono({ subsets: ["latin"], weight: "400" });
 
-export const Banner = () => {
+interface BannerProps {
+  title1: string;
+  title2: string;
+  subTitle: string;
+}
+
+export const Banner: FC<BannerProps> = ({ title1, title2, subTitle }) => {
   return (
     <div className="relative bg-particle-pattern py-32 flex flex-col justify-center items-center">
       <Image
@@ -14,16 +21,16 @@ export const Banner = () => {
       />
       <h1 className="uppercase text-white text-center text-nowrap relative font-tusker-grotesk">
         <div className="text-4xl sm:text-7xl md:text-8xl lg:text-9xl xl:tracking-[-4px]">
-          the future of
+          {title1}
         </div>
         <div className="text-4xl sm:text-7xl md:text-8xl lg:text-9xl xl:tracking-[-2px] font-black text-peach ">
-          tokenization
+          {title2}
         </div>
       </h1>
       <div
         className={`text-white text-lg sm:text-xl lg:text-3xl xl:text-4xl mt-4 ${plex.className}`}
       >
-        q/acc = QF*ABC
+        {subTitle}
       </div>
     </div>
   );
