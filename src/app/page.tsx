ead01358f0b93adb2f70a06a8cbd581a118f6b1f
Cherik <Pourcheriki@gmail.com>
2024-08-15 00:10:01 +0330

    add banner content for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,19 @@
 "use client";
 
 import { Banner } from "@/components/Banner";
-import { Button } from "@/components/Button";
 import Collaborator from "@/components/Collaborator";
 import { FeaturedProjects } from "@/components/FeaturedProjects";
-import { IconArrowRight } from "@/components/Icons/IconArrowRight";
-import { ProjectCard } from "@/components/ProjectCard/ProjectCard";
-import { checkWhiteList } from "@/services/check-white-list";
-import { useWeb3Modal } from "@web3modal/wagmi/react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { useAccount, useConnect, useDisconnect } from "wagmi";
 import About from "@/components/About";
 import InfoSection from "@/components/InfoSection";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  const { open: openWeb3Modal } = useWeb3Modal();
-  const { address } = useAccount();
-  const router = useRouter();
-
   return (
     <main className="">
-      <Banner />
+      <Banner
+        title1="the future of"
+        title2="tokenization"
+        subTitle="q/acc = QF*ABC"
+      />
       <InfoSection />
       <About />
       <FeaturedProjects />
