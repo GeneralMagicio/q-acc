a0e222d1d1f20ab12efd46767f3420831750b91d
Cherik <Pourcheriki@gmail.com>
2024-08-15 00:21:03 +0330

    add home page content of info section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,18 @@ export default function Home() {
         title2="tokenization"
         subTitle="q/acc = QF*ABC"
       />
-      <InfoSection />
+      <InfoSection title="About q/acc">
+        <p>
+          Quadratic Acceleration (q/acc) combines the strengths of Quadratic
+          Funding (QF) with those of Augmented Bonding Curves (ABC) in order to
+          create a new mechanism for launching tokens with built-in liquidity, a
+          passive revenue stream and a clear path to community growth.
+        </p>
+        <p>
+          The Quadratic Accelerator is a collaborative entity under Giveth
+          building the q/acc protocol based on the research of Commons Stack.
+        </p>
+      </InfoSection>
       <About />
       <FeaturedProjects />
       <Collaborator />
