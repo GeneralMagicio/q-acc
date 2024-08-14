7afbcf06326e893fe602f95b44f115f03cae43fa
Cherik <Pourcheriki@gmail.com>
2024-08-15 01:25:42 +0330

    add header items

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
 import { ConnectButton } from "../ConnectButton/ConnectButton";
 import { IconX } from "../Icons/IconX";
+import Routes from "@/lib/constants/Routes";
+import { HeaderItem } from "./HeaderItem";
+
+const HEADER_ITEMS = [
+  {
+    label: "For Donors",
+    route: Routes.Home,
+  },
+  {
+    label: "For Creators",
+    route: Routes.Creator,
+  },
+  {
+    label: "All Projects",
+    route: Routes.Projects,
+  },
+];
 
 export const Header = () => {
   return (
-    <nav className="bg-white flex items-center justify-between p-6 gap-4 z-50 relative">
+    <nav className="bg-white flex items-center p-6 gap-4 z-50 relative">
       <div className="flex gap-4 items-center">
         <Image
           src="/images/icons/logomark-dark.svg"
@@ -20,6 +37,12 @@ export const Header = () => {
           width={48}
         />
       </div>
+      <div className="flex">
+        {HEADER_ITEMS.map((item, index) => (
+          <HeaderItem key={index} label={item.label} route={item.route} />
+        ))}
+      </div>
+      <div className="flex-1"></div>
       <ConnectButton />
     </nav>
   );
