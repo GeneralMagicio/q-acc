b7738c044cddc8e17426270cb884f2db1533ec2c
Cherik <Pourcheriki@gmail.com>
2024-08-18 15:05:54 +0330

    update hold on modal

--- src/components/HoldModal.tsx
+++ src/components/HoldModal.tsx
@@ -7,9 +7,14 @@ interface HoldModalProps extends BaseModalProps {}
 
 export const HoldModal: FC<HoldModalProps> = (props) => {
   return (
-    <Modal {...props} title="Hold On!">
-      <div className="flex flex-col gap-10 ">
-        <p>That address is not on the allowlist.</p>
+    <Modal {...props} title="Hold On">
+      <div className=" ">
+        <h1 className="font-bold text-2xl">Sorry!</h1>
+        <p className="mt-4">
+          The connected address is not on the allow list for any project. Double
+          check you are connected with the right address. If you believe this is
+          in error, reach out the the Quadratic Accelerator team..
+        </p>
       </div>
     </Modal>
   );
