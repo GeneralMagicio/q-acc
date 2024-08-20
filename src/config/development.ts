e990d83011bae9f45735d7805caffe092707fb13
Cherik <Pourcheriki@gmail.com>
2024-08-20 14:33:37 +0330

    add GRAPHQL_ENDPOINT

--- src/config/development.ts
+++ src/config/development.ts
@@ -4,7 +4,7 @@ const config = {
   SUPPORTED_CHAINS: [polygonZkEvmCardona] as const,
   GRAPHQL_ENDPOINT:
     process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
-    "https://impact-graph.serve.giveth.io/graphql",
+    "https://staging.qacc-be.generalmagic.io/graphql",
   AUTH_BASE_ROUTE:
     process.env.NEXT_PUBLIC_AUTH_BASE_ROUTE ||
     "https://auth.serve.giveth.io/v1",
--- src/config/production.ts
+++ src/config/production.ts
@@ -4,7 +4,7 @@ const config = {
   SUPPORTED_CHAINS: [polygonZkEvm] as const,
   GRAPHQL_ENDPOINT:
     process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
-    "https://mainnet.serve.giveth.io/graphql",
+    "https://qacc-be.generalmagic.io/graphql",
   AUTH_BASE_ROUTE:
     process.env.NEXT_PUBLIC_AUTH_BASE_ROUTE || "https://auth.giveth.io/v1",
 };
