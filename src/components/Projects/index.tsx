7420f3d1de50beef91cff4d6bafddbe7b5869552
Cherik <Pourcheriki@gmail.com>
2024-08-22 17:02:29 +0330

    add ProjectsView

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import React from "react";
+import { ProjectsBanner } from "./ProjectsBanner";
+
+export const ProjectsView = () => {
+  return <ProjectsBanner />;
+};
