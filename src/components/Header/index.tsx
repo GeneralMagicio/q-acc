f3b8564762fd53c4bc47af72fc51bff68b978441
Cherik <Pourcheriki@gmail.com>
2024-08-22 13:22:54 +0330

    add link to logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { ConnectButton } from "../ConnectButton/ConnectButton";
 import { IconX } from "../Icons/IconX";
 import Routes from "@/lib/constants/Routes";
 import { HeaderItem } from "./HeaderItem";
+import Link from "next/link";
 
 const HEADER_ITEMS = [
   {
@@ -22,21 +23,23 @@ const HEADER_ITEMS = [
 export const Header = () => {
   return (
     <nav className="bg-white flex items-center p-6 z-50 relative flex-wrap justify-between md:flex-nowrap ">
-      <div className="flex gap-4 items-center order-1">
-        <Image
-          src="/images/icons/logomark-dark.svg"
-          alt="logo"
-          width={87}
-          height={40}
-        />
-        <IconX size={14} />
-        <Image
-          src="/images/icons/giveth.svg"
-          alt="giveth-logo"
-          height={48}
-          width={48}
-        />
-      </div>
+      <Link href={Routes.Home}>
+        <div className="flex gap-4 items-center order-1">
+          <Image
+            src="/images/icons/logomark-dark.svg"
+            alt="logo"
+            width={87}
+            height={40}
+          />
+          <IconX size={14} />
+          <Image
+            src="/images/icons/giveth.svg"
+            alt="giveth-logo"
+            height={48}
+            width={48}
+          />
+        </div>
+      </Link>
       <div className="flex order-3 flex-1 justify-between md:order-2 md:flex-grow-0">
         {HEADER_ITEMS.map((item, index) => (
           <HeaderItem key={index} label={item.label} route={item.route} />
