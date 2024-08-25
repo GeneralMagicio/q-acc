77ed6e4cd6dd1f13846be28bd800cdf1bb6003f0
Cherik <Pourcheriki@gmail.com>
2024-08-25 23:41:01 +0330

    add GET_GIVETH_USER_BY_ADDRESS

--- src/components/ConnectButton/query.ts
+++ src/components/ConnectButton/query.ts
@@ -4,6 +4,21 @@ export const GET_USER_BY_ADDRESS = `
 			__typename
 			id
 			avatar
+			email
+		}
+	}
+`;
+
+export const GET_GIVETH_USER_BY_ADDRESS = `
+	query UserByAddress($address: String!) {
+		userByAddress(address: $address) {
+			__typename
+			id
+			avatar
+			email
+			firstName
+			lastName
+			name
 		}
 	}
 `;
