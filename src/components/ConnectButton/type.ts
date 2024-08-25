5dcbdf3713a6ea5f909a03a526a828a8100b014f
Cherik <Pourcheriki@gmail.com>
2024-08-25 23:40:34 +0330

    add IGivethUser

--- src/components/ConnectButton/type.ts
+++ src/components/ConnectButton/type.ts
@@ -1,4 +1,15 @@
-interface IUser {
+export interface IUser {
   id: string;
+  fullName: string;
+  email: string;
+  avatar: string;
+}
+
+export interface IGivethUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  name: string;
+  email: string;
   avatar: string;
 }
