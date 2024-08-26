3d1406aab01f4d0e547b41ed0ef8746605a49971
Cherik <Pourcheriki@gmail.com>
2024-08-26 11:34:47 +0330

    add useFetchUser

---
+++ src/hooks/useFetchUser.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAccount } from "wagmi";
+import { fetchUserInfo } from "@/services/user.service";
+
+export const useFetchUser = () => {
+  const { address } = useAccount();
+
+  return useQuery({
+    queryKey: ["user", address],
+    queryFn: async () => {
+      if (!address) return;
+      return await fetchUserInfo(address);
+    },
+    enabled: !!address,
+    staleTime: Infinity,
+    gcTime: Infinity,
+  });
+};
