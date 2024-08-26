bea841df80fe850fa52de7e8de442fb6b3bfc407
Cherik <Pourcheriki@gmail.com>
2024-08-26 11:33:53 +0330

    add user types

---
+++ src/types/user.type.ts
@@ -0,0 +1,15 @@
+export interface IUser {
+  id: string;
+  fullName: string;
+  email: string;
+  avatar: string;
+}
+
+export interface IGivethUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  name: string;
+  email: string;
+  avatar: string;
+}
