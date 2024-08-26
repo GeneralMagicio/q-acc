fb460c53e26be26ea21d899083476b726ccf5496
Cherik <Pourcheriki@gmail.com>
2024-08-26 14:15:08 +0330

    add projects card

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,7 +1,28 @@
 import Image from "next/image";
 import React from "react";
 import { ProjectsBanner } from "./ProjectsBanner";
+import { ProjectCard } from "../ProjectCard/ProjectCard";
+
+const projectCardStyle = "";
 
 export const ProjectsView = () => {
-  return <ProjectsBanner />;
+  return (
+    <>
+      <ProjectsBanner />
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 py-10">
+          <ProjectCard className={projectCardStyle} />
+          <ProjectCard className={projectCardStyle} />
+          <ProjectCard className={projectCardStyle} />
+          <ProjectCard className={projectCardStyle} />
+          <ProjectCard className={projectCardStyle} />
+          <ProjectCard className={projectCardStyle} />
+          <ProjectCard className={projectCardStyle} />
+          <ProjectCard className={projectCardStyle} />
+          <ProjectCard className={projectCardStyle} />
+          <ProjectCard className={projectCardStyle} />
+        </div>
+      </div>
+    </>
+  );
 };
