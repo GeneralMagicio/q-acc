28b4a2b96a6fc22d14672de642c3f9728cded79f
Cherik <Pourcheriki@gmail.com>
2024-08-26 11:54:45 +0330

    add useUpdateUser

---
+++ src/hooks/useUpdateUser.ts
@@ -0,0 +1,27 @@
+import { requestGraphQL } from "@/helpers/request";
+import { UPDATE_USER } from "@/queries/user.query";
+import { INewUer } from "@/types/user.type";
+import { useMutation } from "@tanstack/react-query";
+import { useAccount } from "wagmi";
+
+export const useUpdateUser = () => {
+  const { address } = useAccount();
+  return useMutation({
+    mutationKey: ["user", address],
+    mutationFn: async (user: INewUer) => {
+      console.log("Mutation function called");
+      await requestGraphQL(
+        UPDATE_USER,
+        {
+          email: user.email,
+          fullName: user.fullName,
+          avatar: user.avatar,
+          newUser: true,
+        },
+        {
+          auth: true,
+        }
+      );
+    },
+  });
+};
