73ee1d6ae8c270b8860cf5c74bc2e2fa9da9e819
Cherik <Pourcheriki@gmail.com>
2024-08-27 15:03:10 +0330

    fix prettier

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -5,16 +5,16 @@ import { CREATE_PROJECT } from '@/queries/project.query';
 import type { IGivethUser } from '@/types/user.type';
 
 export const createProject = async (project: IProjectCreation) => {
-	try {
-		const res = await requestGraphQL<{ userByAddress: IGivethUser }>(
-			CREATE_PROJECT,
-			{ project },
-			{
-				url: config.GIVETH_GQL_ENDPOINT,
-			},
-		);
-		return res?.userByAddress;
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    const res = await requestGraphQL<{ userByAddress: IGivethUser }>(
+      CREATE_PROJECT,
+      { project },
+      {
+        url: config.GIVETH_GQL_ENDPOINT,
+      },
+    );
+    return res?.userByAddress;
+  } catch (error) {
+    console.error(error);
+  }
 };
