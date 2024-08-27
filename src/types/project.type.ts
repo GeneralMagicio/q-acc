4d49b84ebaa46b59f93c31076a18cd30fe7bd34f
Cherik <Pourcheriki@gmail.com>
2024-08-27 14:25:48 +0330

    init project type

---
+++ src/types/project.type.ts
@@ -0,0 +1,87 @@
+export enum ChainType {
+	SOLANA = 'SOLANA',
+	EVM = 'EVM',
+}
+
+export interface IWalletAddress {
+	address?: string;
+	isRecipient?: boolean;
+	networkId?: number;
+	chainType?: ChainType;
+}
+
+export interface IAdminUser {
+	id?: string;
+	email?: string;
+	name?: string;
+	walletAddress?: string;
+	avatar?: string;
+}
+
+export enum EProjectSocialMediaType {
+	FACEBOOK = 'FACEBOOK',
+	X = 'X',
+	INSTAGRAM = 'INSTAGRAM',
+	YOUTUBE = 'YOUTUBE',
+	LINKEDIN = 'LINKEDIN',
+	REDDIT = 'REDDIT',
+	DISCORD = 'DISCORD',
+	FARCASTER = 'FARCASTER',
+	LENS = 'LENS',
+	WEBSITE = 'WEBSITE',
+	TELEGRAM = 'TELEGRAM',
+	GITHUB = 'GITHUB',
+}
+
+export interface IProjectSocialMedia {
+	type: EProjectSocialMediaType;
+	link: string;
+}
+
+export interface IProjectCreation {
+	title: string;
+	description: string;
+	impactLocation?: any;
+	categories: any;
+	organisationId: number;
+	walletAddress?: string;
+	addresses?: IWalletAddress[];
+	image?: string;
+	isDraft?: boolean;
+	socialMedia?: IProjectSocialMedia[];
+}
+
+export interface IProject {
+	id: string;
+	title?: string;
+	balance?: number;
+	image?: string;
+	slug: string;
+	creationDate?: string;
+	adminUserId?: number;
+	description?: string;
+	descriptionSummary?: string;
+	addresses?: IWalletAddress[];
+	impactLocation?: string;
+	qualityScore?: number;
+	verified?: boolean;
+	adminUser: IAdminUser;
+	donations: {
+		id?: string;
+	}[];
+	totalDonations?: number;
+	totalProjectUpdates?: number;
+	updatedAt: string;
+	latestUpdateCreationDate?: string;
+	organization?: {
+		name: string;
+		label: string;
+		supportCustomTokens: boolean;
+		disableRecurringDonations?: boolean;
+	};
+	givbackFactor?: number;
+	countUniqueDonors?: number;
+	countUniqueDonorsForActiveQfRound?: number;
+	sumDonationValueUsdForActiveQfRound?: number;
+	socialMedia: IProjectSocialMedia[];
+}
