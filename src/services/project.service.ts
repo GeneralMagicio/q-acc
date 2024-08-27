6c249c2eecdaf0e746ef0c2005be1d8c3039ca7f
Cherik <Pourcheriki@gmail.com>
2024-08-27 14:25:59 +0330

    init project service

---
+++ src/services/project.service.ts
@@ -0,0 +1,20 @@
+import { requestGraphQL } from '@/helpers/request';
+import config from '@/config/configuration';
+import { IProjectCreation } from '@/types/project.type';
+import { CREATE_PROJECT } from '@/queries/project.query';
+import type { IGivethUser } from '@/types/user.type';
+
+export const createProject = async (project: IProjectCreation) => {
+	try {
+		const res = await requestGraphQL<{ userByAddress: IGivethUser }>(
+			CREATE_PROJECT,
+			{ project },
+			{
+				url: config.GIVETH_GQL_ENDPOINT,
+			},
+		);
+		return res?.userByAddress;
+	} catch (error) {
+		console.error(error);
+	}
+};
