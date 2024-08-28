be7d8240ba1b19949f1097156647424639149acc
Cherik <Pourcheriki@gmail.com>
2024-08-28 16:50:20 +0330

    remove unused link

--- src/components/Modals/SignModal.tsx
+++ src/components/Modals/SignModal.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import Modal, { BaseModalProps } from '../Modal';
 import { Button, ButtonColor, ButtonStyle } from '../Button';
 import { IconArrowRight } from '../Icons/IconArrowRight';
-import Routes from '@/lib/constants/Routes';
 import { useSignUser } from '@/hooks/useSignUser';
 import type { FC } from 'react';
 
@@ -21,19 +19,17 @@ export const SignModal: FC<SignModalProps> = props => {
         <p className='mt-4 mb-10'>
           Please sign the login message with your wallet to continue.
         </p>
-        <Link href={Routes.CreateProfile}>
-          <Button
-            styleType={ButtonStyle.Solid}
-            color={ButtonColor.Pink}
-            onClick={() => refetch()}
-            loading={isFetching}
-          >
-            <div className='flex items-center gap-1'>
-              <span>Authenticate Wallet</span>
-              <IconArrowRight size={16} />
-            </div>
-          </Button>
-        </Link>
+        <Button
+          styleType={ButtonStyle.Solid}
+          color={ButtonColor.Pink}
+          onClick={() => refetch()}
+          loading={isFetching}
+        >
+          <div className='flex items-center gap-1'>
+            <span>Authenticate Wallet</span>
+            <IconArrowRight size={16} />
+          </div>
+        </Button>
         <Image
           src='/images/particles/trazado-v-b.svg'
           alt='trazado'
