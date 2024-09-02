ac109263c19fedb09796fa4d480b634a02bd4534
Cherik <Pourcheriki@gmail.com>
2024-09-02 18:25:57 +0330

    use real projects in projects page

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { ProjectsBanner } from './ProjectsBanner';
 import { ProjectCard } from '../ProjectCard/ProjectCard';
+import { fetchAllProjects } from '@/services/project.service';
 
 const projectCardStyle = '';
 
 export const ProjectsView = () => {
+  const { data: allProjects } = useQuery({
+    queryKey: ['allProjects'],
+    queryFn: fetchAllProjects,
+    gcTime: Infinity,
+    staleTime: Infinity,
+  });
   return (
     <>
       <ProjectsBanner />
       <div className='container mx-auto'>
         <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 py-10'>
-          <ProjectCard className={projectCardStyle} />
-          <ProjectCard className={projectCardStyle} />
-          <ProjectCard className={projectCardStyle} />
-          <ProjectCard className={projectCardStyle} />
-          <ProjectCard className={projectCardStyle} />
-          <ProjectCard className={projectCardStyle} />
-          <ProjectCard className={projectCardStyle} />
-          <ProjectCard className={projectCardStyle} />
-          <ProjectCard className={projectCardStyle} />
-          <ProjectCard className={projectCardStyle} />
+          {allProjects?.projects?.map(project => (
+            <ProjectCard
+              key={project.id}
+              project={project}
+              className={projectCardStyle}
+            />
+          ))}
         </div>
       </div>
     </>
