098b78b68c8c8fc98791e877329de5a94c531541
Amin Latifi <a.latifi.al@gmail.com>
2024-09-09 02:08:41 +0330

    update type

--- src/types/config.type.ts
+++ src/types/config.type.ts
@@ -4,6 +4,7 @@ export type EnvConfig = {
   SUPPORTED_CHAINS: readonly [Chain, ...Chain[]];
   GRAPHQL_ENDPOINT: string;
   AUTH_BASE_ROUTE: string;
+  SCAN_URL: string;
   GIVETH_GQL_ENDPOINT: string;
   NETWORK_RPC_ADDRESS: string;
   privadoConfig: {
