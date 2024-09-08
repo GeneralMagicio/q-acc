7c84926a3c7349835c6c42413f192166a5065ec6
Amin Latifi <a.latifi.al@gmail.com>
2024-09-09 02:03:18 +0330

    Fixed verify-privado page style and texts

--- src/components/Create/VerifyPrivado/index.tsx
+++ src/components/Create/VerifyPrivado/index.tsx
@@ -38,11 +38,6 @@ const VerifyPrivado = () => {
         disabled={!verified.isVerified}
       />
       <div className='w-full bg-white flex flex-col p-8 gap-10 rounded-2xl mt-6'>
-        <div>
-          <h1 className='text-2xl text-[#1D1E1F] font-bold'>
-            Account Verification
-          </h1>
-        </div>
         <div className='flex flex-col gap-8'>
           <div className='font-redHatText'>
             <p className='text-[#1D1E1F] text-xl'>
@@ -50,6 +45,31 @@ const VerifyPrivado = () => {
               acceleration. All participants are required to validate their
               credentials with out identity partner, Privado.
             </p>
+          </div>
+          <div className='font-redHatText'>
+            <p className='text-gray-900 font-medium'>Here is how it works:</p>
+          </div>
+          <div className='bg-[#F7F7F9] flex flex-col gap-6 p-6 rounded-2xl'>
+            <div className='flex flex-col gap-4'>
+              <h6 className='text-gray-900 font-bold'>
+                1. Click the button below to go to Privado.
+              </h6>
+              <p className='text-gray-800 font-normal'>
+                A new tab will open where you can complete the verification
+                steps.
+              </p>
+            </div>
+          </div>
+          <div className='bg-[#F7F7F9] flex flex-col gap-6 p-6 rounded-2xl'>
+            <div className='flex flex-col gap-4'>
+              <h6 className='text-gray-900 font-bold'>
+                2. Verification may take up to 24 hours.
+              </h6>
+              <p className='text-gray-800 font-normal'>
+                Once your credentials are validated, we will show the success
+                status on your q/acc profile page.
+              </p>
+            </div>
           </div>
           <div className='flex flex-col gap-10 lg:flex-row justify-between'>
             {!isLoading ? (
@@ -81,7 +101,8 @@ const VerifyPrivado = () => {
                   <>
                     <IconInfo size={17} color='#8668FC' />
                     <span className='text-[#8668FC] font-redHatText text-sm'>
-                      Identity credentials are required.
+                      You are not able to participate in q/acc until you have
+                      validated your credentials with Privado.
                     </span>
                   </>
                 )}
@@ -119,7 +140,19 @@ const VerifyPrivado = () => {
                   onClick={verifyAccount}
                   className='p-4 rounded-full shadow-baseShadow text-sm font-bold min-w-[200px] justify-center'
                 >
-                  {isLoading ? 'Loading' : 'Verify My Account'}
+                  {isLoading ? (
+                    'Loading'
+                  ) : (
+                    <>
+                      Go to Privado
+                      <Image
+                        src='/images/icons/arrowPurple.svg'
+                        alt='tick'
+                        height={16}
+                        width={16}
+                      />
+                    </>
+                  )}
                 </Button>
               )}
             </div>
