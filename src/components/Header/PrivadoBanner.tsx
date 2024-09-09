0af08e00df180b0e4113c8918d1b5d62f31ad7cd
Amin Latifi <a.latifi.al@gmail.com>
2024-09-09 18:52:24 +0330

    Added Privado Banner

---
+++ src/components/Header/PrivadoBanner.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import { usePrivado } from '@/hooks/usePrivado';
+
+export const PrivadoBanner = () => {
+  const { isVerified, isLoading, verifyAccount } = usePrivado();
+  const showPrivadoBanner = !isVerified && !isLoading;
+
+  return (
+    <>
+      {showPrivadoBanner && (
+        <div
+          className='w-full h-14 bg-[#FFEAB5] px-8 py-4 flex flex-row justify-center gap-2 items-center relative'
+          hidden={!showPrivadoBanner}
+        >
+          <div className='w-1/2 flex justify-end gap-2'>
+            <Image
+              src='/images/icons/info.svg'
+              alt='info'
+              height={24}
+              width={24}
+            />
+            <p className='text-gray-900 font-normal font-redHatText'>
+              You don’t have identity credentials setup.
+            </p>
+          </div>
+          <div
+            className='flex flex-row gap-2 w-1/2 justify-start cursor-pointer'
+            onClick={verifyAccount}
+          >
+            <p className='text-giv-500'>Check status with Privado</p>
+            <Image
+              src='/images/icons/external-link.svg'
+              alt='privado-verify'
+              height={12}
+              width={12}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
