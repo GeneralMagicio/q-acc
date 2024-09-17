d05ce3e46409a3bea0bb7d19975a33d41a280d55
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2024-09-17 08:08:34 -0400

    change to base sepolia network

--- src/config/production.ts
+++ src/config/production.ts
@@ -3,14 +3,15 @@ import { EnvConfig } from '@/types/config.type';
 
 const config: EnvConfig = {
   SUPPORTED_CHAINS: [polygonZkEvm] as const,
-  SCAN_URL: 'https://zkevm.polygonscan.com/',
+  SCAN_URL: 'https://basescan.org/',
+  ERC_TOKEN_ADDRESS: '0x9Cc1c42e4525ba4116d7948741ccA9C7446596b2',
   GRAPHQL_ENDPOINT:
     process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
     'https://qacc-be.generalmagic.io/graphql',
   AUTH_BASE_ROUTE:
     process.env.NEXT_PUBLIC_AUTH_BASE_ROUTE || 'https://auth.giveth.io/v1',
   GIVETH_GQL_ENDPOINT: 'https://mainnet.serve.giveth.io/graphql',
-  NETWORK_RPC_ADDRESS: 'https://rpc.ankr.com/polygon_zkevm_cardona',
+  NETWORK_RPC_ADDRESS: 'https://mainnet.base.org',
   privadoConfig: {
     chain: polygonZkEvm,
     contractAddress: '0x',
