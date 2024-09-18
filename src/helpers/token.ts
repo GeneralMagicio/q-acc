53218967c35881c2bbc8f0c79561d62b5b75f9c8
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2024-09-18 08:02:59 -0400

    changed rpc endpoint

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -2,6 +2,7 @@ import { writeContract } from '@wagmi/core';
 import { ethers } from 'ethers';
 import { erc20Abi, formatUnits, parseUnits } from 'viem';
 import { wagmiConfig } from '@/config/wagmi';
+import config from '@/config/configuration';
 
 export const fetchTokenDetails = async ({
   tokenAddress,
@@ -79,7 +80,7 @@ export const checkUserOwnsNFT = async (
   nftContractAddress: string,
   userAddress: string,
 ) => {
-  const url = 'https://sepolia.optimism.io';
+  const url = config.NETWORK_RPC_ADDRESS;
   const provider = new ethers.JsonRpcProvider(url);
 
   const abi = [
