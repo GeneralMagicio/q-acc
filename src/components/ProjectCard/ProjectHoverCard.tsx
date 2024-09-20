3d42a0f7c8730db91a6ee063c9510729b0b90c1d
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2024-09-20 10:31:51 -0400

    added logic to progress bar with max amount

--- src/components/ProjectCard/ProjectHoverCard.tsx
+++ src/components/ProjectCard/ProjectHoverCard.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAccount } from 'wagmi';
 import { IProject } from '@/types/project.type';
@@ -7,9 +7,11 @@ import ProjectCardImage from './ProjectCardImage';
 
 import { Button, ButtonColor } from '../Button';
 import { getIpfsAddress } from '@/helpers/image';
-import { checkUserOwnsNFT } from '@/helpers/token';
+import { checkUserOwnsNFT, fetchTokenPrice } from '@/helpers/token';
 import { NFTModal } from '../Modals/NFTModal';
 import ProgressBar from '../ProgressBar';
+import { fecthProjectDonationsById } from '@/services/donation.services';
+import { calculateTotalDonations } from '@/helpers/donation';
 
 interface ProjectCardProps extends React.HTMLAttributes<HTMLDivElement> {
   project: IProject;
@@ -24,9 +26,37 @@ export const ProjectHoverCard: FC<ProjectCardProps> = ({
   const router = useRouter();
   const { address } = useAccount();
   const [isModalOpen, setModalOpen] = useState(false);
+  const [totalAmount, setTotalAmount] = useState<number>(0);
+  const [tokenPrice, setTokenPrice] = useState(1);
 
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
+  useEffect(() => {
+    const fetchPrice = async () => {
+      const price = await fetchTokenPrice('wmatic');
+      setTokenPrice(price);
+    };
+
+    fetchPrice();
+  }, []);
+  useEffect(() => {
+    if (project?.id) {
+      const fetchProjectDonations = async () => {
+        const data = await fecthProjectDonationsById(
+          parseInt(project?.id),
+          1000,
+          0,
+        );
+
+        if (data) {
+          const { donations, totalCount } = data;
+
+          setTotalAmount(calculateTotalDonations(donations));
+        }
+      };
+      fetchProjectDonations();
+    }
+  }, [project]);
 
   const handleSupport = async (e: any) => {
     e.stopPropagation();
@@ -48,15 +78,11 @@ export const ProjectHoverCard: FC<ProjectCardProps> = ({
     router.push(`/project/${project.slug}`);
   };
 
-  console.log(project, 'data');
+  console.log(project, 'data', totalAmount);
+  const maxedAmount = 100000; //100k dollars later from backend
 
-  let progress = 67;
-  if (project?.id === '53') {
-    progress = 100;
-  }
-  if (project?.id === '55') {
-    progress = 0;
-  }
+  let progress =
+    Math.round(((totalAmount * tokenPrice) / maxedAmount) * 100 * 100) / 100;
 
   return (
     <div
