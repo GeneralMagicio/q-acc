e29c8f10b0a33491da1fa939b70a6e6781e0e5e8
Cherik <Pourcheriki@gmail.com>
2024-09-22 15:32:06 +0330

    update useRemainingTime to accept string

--- src/hooks/useRemainingTime.ts
+++ src/hooks/useRemainingTime.ts
@@ -2,16 +2,17 @@
 import { useState, useEffect } from 'react';
 import { calculateRemainingTime } from '@/helpers/date';
 
-function useRemainingTime(endDate: Date) {
+function useRemainingTime(endDate: string) {
   const [remainingTime, setRemainingTime] = useState<string>('');
 
   useEffect(() => {
+    const _endDate = new Date(endDate);
     // Set initial remaining time
-    setRemainingTime(calculateRemainingTime(endDate));
+    setRemainingTime(calculateRemainingTime(_endDate));
 
     // Update remaining time every second
     const interval = setInterval(() => {
-      setRemainingTime(calculateRemainingTime(endDate));
+      setRemainingTime(calculateRemainingTime(_endDate));
     }, 1000);
 
     // Cleanup interval on component unmount
