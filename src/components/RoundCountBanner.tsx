10104c2b6564fb3b0db619485e424269c5d82af9
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2024-09-23 16:27:26 -0400

    fixed build

--- src/components/RoundCountBanner.tsx
+++ src/components/RoundCountBanner.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useFetchRoundDetails } from '@/hooks/useFetchRoundDetails';
 import useRemainingTime from '@/hooks/useRemainingTime';
 
