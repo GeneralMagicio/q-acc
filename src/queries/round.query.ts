a62806cabc37294d47cbe40e037d61fb7d6c6c1c
Cherik <Pourcheriki@gmail.com>
2024-09-24 15:26:38 +0330

    add GET_ALL_ROUNDS query

--- src/queries/round.query.ts
+++ src/queries/round.query.ts
@@ -1,3 +1,24 @@
+export const GET_ALL_ROUNDS = `
+  query {
+    allRounds {
+      ... on EarlyAccessRound {
+        roundNumber
+        startDate
+        endDate
+        createdAt
+        updatedAt
+      }
+      ... on QfRound {
+        name
+        slug
+        allocatedFund
+        beginDate
+        endDate
+      }
+    }
+  }
+`;
+
 export const GET_ACTIVE_ROUND = `
   query {
     activeRound {
