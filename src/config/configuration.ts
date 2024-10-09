2488eba44c1592952cc8d42a286ab491cec8dfd8
Cherik <Pourcheriki@gmail.com>
2024-10-09 17:34:33 +0330

    add isProductReleased

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,6 +2,8 @@ import development from './development';
 import production from './production';
 
 export const isProduction = process.env.NEXT_PUBLIC_ENV === 'production';
+export const isProductReleased =
+  process.env.NEXT_PUBLIC_IS_PRODUCT_RELEASE === 'true';
 
 const envConfig = isProduction ? production : development;
 const config = {
