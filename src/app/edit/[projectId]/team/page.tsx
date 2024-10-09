ce999031122af9a8859c39f17f1258ebc6bd8b7a
Cherik <Pourcheriki@gmail.com>
2024-10-09 18:29:51 +0330

    redirect edit team page if Product is not Released

--- src/app/edit/[projectId]/team/page.tsx
+++ src/app/edit/[projectId]/team/page.tsx
@@ -1,17 +1,21 @@
 'use client';
 import React from 'react';
 
-import { useParams } from 'next/navigation';
+import { redirect, useParams } from 'next/navigation';
 import EditTeamForm from '@/components/EditForm/EditTeam';
+import { isProductReleased } from '@/config/configuration';
+import Routes from '@/lib/constants/Routes';
 
 export default function EditTeamPage() {
   const params = useParams();
   const projectId: number = Array.isArray(params.projectId)
     ? parseInt(params.projectId[0])
     : parseInt(params.projectId);
-  return (
+  return isProductReleased ? (
     <div>
       <EditTeamForm projectId={projectId} />
     </div>
+  ) : (
+    redirect(Routes.KycLanding)
   );
 }
