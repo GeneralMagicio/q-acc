f461879d2e456043dc282b236761febf878d34af
Amin Latifi <a.latifi.al@gmail.com>
2024-10-11 13:43:17 +0330

    Updated verifier contract

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,6 +3,10 @@ import production from './production';
 
 export const isProduction = process.env.NEXT_PUBLIC_ENV === 'production';
 
+if (!isProduction) {
+  console.log('Running in development mode');
+}
+
 const envConfig = isProduction ? production : development;
 const config = {
   ...envConfig,
