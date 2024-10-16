8a83912ded4421b5c6784e34939a83e1ffc6710f
Cherik <Pourcheriki@gmail.com>
2024-10-16 15:46:22 +0330

    fix the box style

--- src/components/KycCompleteBox.tsx
+++ src/components/KycCompleteBox.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const KycCompleteBox = () => {
   return (
-    <div className='py-6 px-4 shadow-cardShadow'>
+    <div className='p-8 shadow-baseShadow rounded-xl relative bg-white'>
       <h1 className='text-3xl text-gray-800 font-bold'>KYC Complete</h1>
       <p className='text-gray-600  border-b-[1px]'>
         Your identity verification with Privado ID was successful.
