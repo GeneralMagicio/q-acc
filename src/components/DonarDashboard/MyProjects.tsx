31178700c95d685a0f64f1d6a362fe20e6cdd228
Amin Latifi <a.latifi.al@gmail.com>
2024-10-17 19:43:39 +0330

    Fixed linting issue

--- src/components/DonarDashboard/MyProjects.tsx
+++ src/components/DonarDashboard/MyProjects.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useAccount } from 'wagmi';
 import Link from 'next/link';
 import Image from 'next/image';
 import { IconViewTransaction } from '../Icons/IconViewTransaction';
