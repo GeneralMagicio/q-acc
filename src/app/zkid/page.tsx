33fb02abf5daea7d6cbe624d904a7ccdfc2c5235
Cherik <Pourcheriki@gmail.com>
2024-11-05 14:13:39 +0330

    update copy

--- src/app/zkid/page.tsx
+++ src/app/zkid/page.tsx
@@ -32,40 +32,98 @@ const KycLanding = () => {
               Get ready for Season 1
             </h1>
             <p>
-              We use Privado ID zero knowledge identity system to maintain the
-              integrity of quadratic acceleration while ensuring privacy. All
-              participants are required to complete verification in order to
-              receive a KYC credential from the Synaps identity provider.
+              The q/acc protocol uses Privado ID zero knowledge system to
+              maintain integrity and ensure privacy. Participants receive their
+              KYC credential from the Synaps identity verification provider.
+              Whether you have an existing Giveth profile or are creating a new
+              one, you’ll need to complete your{' '}
+              <strong className='font-bold'>
+                zero-knowledge identity verification (zkID)
+              </strong>{' '}
+              .
             </p>
             <br />
+            <p>The process is simple:</p>
+            <br />
+            <ul className='list-disc px-10'>
+              <li>
+                <strong className='font-bold'>Liveness Check:</strong> This step
+                confirms you’re a real human.
+              </li>
+              <li>
+                <strong className='font-bold'>Documentation Check:</strong> This
+                step ensures you are not a minor and prevents AML-listed
+                countries from participating, as well as those from the US or UK
+                for regulatory reasons.
+              </li>
+            </ul>
+            <br />
             <p>
-              This quick and simple process protects against Sybil attacks and
-              ensures that prohibited regions are excluded.
+              Once verified, your wallet will hold your{' '}
+              <strong className='font-bold'>KYC credential</strong>, which will
+              allow you to participate without sharing personal data. This easy
+              process protects against Sybil attacks and strengthens regulatory
+              compliance.
             </p>
           </div>
 
           <div className='flex flex-col p-6 gap-6 rounded-2xl bg-gray-100'>
             <h1 className='text-gray-800 text-2xl font-bold'>
-              Add KYC credentials to your account
+              Add zkID credential to your account
             </h1>
             <p>
               If you don’t have an account, you will be prompted to create one.
             </p>
             <ol className='list-decimal pl-6'>
               <li className='mb-8'>
-                Click the <strong>“Get started”</strong> button 
+                Click the “Get started” button to create your profile first.
+              </li>
+              <li className='mb-8'>
+                On the identity verification page, click the “Go to Privado ID”
+                button.
                 <p>
-                  A new tab will open where you can complete the verification
-                  steps.
+                  A new tab will open where you can complete the verification.
+                  Read this{' '}
+                  <a
+                    href='https://giveth.notion.site/Complete-zkID-via-Privado-ID-1223ab28d48c80458699d18cb0f54494'
+                    target='_blank'
+                    className='font-bold text-pink-500'
+                  >
+                    guide
+                  </a>{' '}
+                  for the step-by-step process.
                 </p>
               </li>
               <li className='mb-8'>
-                Check back again in 24 hours and claim your credentials. Once
-                you have passed KYC and claimed your credentials, your status
-                will be displayed on your profile page.
+                <strong className='font-bold'>
+                  THIS MAY TAKE UP TO 24 hours, but it is usually done quickly.
+                </strong>
+                <p>
+                  <strong className='font-bold'>
+                    You can come back here to this site to check the status.
+                  </strong>
+                </p>
+                <p>
+                  Once you have passed zkID verification and claimed your
+                  credentials, your “Verified” status will be displayed on your
+                  profile page.
+                </p>
               </li>
             </ol>
           </div>
+          <p>
+            Got stuck somewhere in the process? If you’re having trouble or
+            received an error, contact the{' '}
+            <a
+              href='https://giveth.notion.site/Complete-zkID-via-Privado-ID-1223ab28d48c80458699d18cb0f54494'
+              target='_blank'
+              className='font-bold text-pink-500'
+            >
+              q/acc Support Telegram channel
+            </a>{' '}
+            or send an email to qacc@giveth.io. We’ll help you get back on
+            track!
+          </p>
           {!isVerified && (
             <div>
               <KycLandingButton />
