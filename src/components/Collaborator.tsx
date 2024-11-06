4530d80e39aa85a7df6c010258dc1c0a506d596b
Cherik <Pourcheriki@gmail.com>
2024-11-06 12:59:51 +0330

    remove logs

--- src/components/Collaborator.tsx
+++ src/components/Collaborator.tsx
@@ -54,34 +54,6 @@ const Collaborator = () => {
             ))}
           </div>
         ))}
-        {/* <div className='flex flex-col md:flex-row items-center justify-center gap-14'>
-          <div className='flex items-center gap-10'>
-            <p className='text-[#A5ADBF] font-bold text-lg leading-normal tracking-tight'>
-              Incubated by
-            </p>
-            <LogoGiveth />
-          </div>
-          <div className='flex items-center gap-10'>
-            <p className='text-[#A5ADBF] font-bold text-lg leading-normal tracking-tight '>
-              Powered by
-            </p>
-            <LogoPolygon />
-          </div>
-        </div>
-
-        <div className='w-full border-t border-gray-300' />
-
-        <div className='flex flex-col md:flex-row items-center justify-center md:gap-20 gap-10'>
-          <p className='text-[#A5ADBF] font-bold text-lg leading-normal tracking-tight'>
-            In collaboration with
-          </p>
-
-          <div className='flex flex-wrap gap-10 items-center justify-center'>
-            <LogoCommonStack />
-            <LogoGM />
-            <LogoInverter />
-          </div>
-        </div> */}
       </div>
     </div>
   );
