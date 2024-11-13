eb7211e0c65b7f98d97dbcd86bf6eb1df634372e
Cherik <Pourcheriki@gmail.com>
2024-11-13 19:25:13 +0330

    change-ttl-to-1-year

--- src/app/actions/generate-privado-uuid.ts
+++ src/app/actions/generate-privado-uuid.ts
@@ -11,24 +11,49 @@ export async function generatePrivadoUuid(data: object) {
     const uuid = uuidv4();
     const db = await getMongoDB();
 
+    const collection = db.collection<IPrivadoStoredData>(
+      PRIVADO_LINK_COLLECTION_NAME,
+    );
+
     // Check if the collection exists
     const collections = await db
       .listCollections({ name: PRIVADO_LINK_COLLECTION_NAME })
       .toArray();
+
+    // Define the TTL value for one year
+    const oneYearInSeconds = 365 * 24 * 60 * 60; // 31,536,000 seconds
 
     if (collections.length === 0) {
       // Collection does not exist, create it
       await db.createCollection(PRIVADO_LINK_COLLECTION_NAME);
 
       // Create TTL index on 'createdAt' field
-      await db
-        .collection(PRIVADO_LINK_COLLECTION_NAME)
-        .createIndex({ createdAt: 1 }, { expireAfterSeconds: 3600 });
-    }
+      await collection.createIndex(
+        { createdAt: 1 },
+        { expireAfterSeconds: oneYearInSeconds },
+      );
+    } else {
+      // Check if the TTL index exists and update it if necessary
+      const indexes = await collection.indexInformation({ full: true });
+      const ttlIndex = indexes.find(
+        (index: any) => index.name === 'createdAt_1',
+      );
 
-    const collection = db.collection<IPrivadoStoredData>(
-      PRIVADO_LINK_COLLECTION_NAME,
-    );
+      if (!ttlIndex) {
+        // Index does not exist, create it
+        await collection.createIndex(
+          { createdAt: 1 },
+          { expireAfterSeconds: oneYearInSeconds },
+        );
+      } else if (ttlIndex.expireAfterSeconds !== oneYearInSeconds) {
+        // Index exists but with a different TTL, recreate it
+        await collection.dropIndex('createdAt_1');
+        await collection.createIndex(
+          { createdAt: 1 },
+          { expireAfterSeconds: oneYearInSeconds },
+        );
+      }
+    }
 
     // Insert the data with a 'createdAt' field
     await collection.insertOne({
