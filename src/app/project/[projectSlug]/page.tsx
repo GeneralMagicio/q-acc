24d4b724e76c5692abc98fc944fb892c61dd5a01
Cherik <Pourcheriki@gmail.com>
2024-11-13 15:30:42 +0330

    make page server component

--- src/app/project/[projectSlug]/page.tsx
+++ src/app/project/[projectSlug]/page.tsx
@@ -1,23 +1,32 @@
-'use client';
-import { redirect, useParams } from 'next/navigation';
-import ProjectDetail from '@/components/ProjectDetail/ProjectDetail';
-import { ProjectProvider } from '@/context/project.context';
+import { redirect } from 'next/navigation';
 import { isProductReleased } from '@/config/configuration';
 import Routes from '@/lib/constants/Routes';
+import { fetchProjectBySlug } from '@/services/project.service';
+import { ProjectView } from '@/components/ProjectDetail/ProjectView';
 
-const ProjectRoute = () => {
-  const params = useParams();
+interface ProjectPageProps {
+  params: {
+    projectSlug: string | string[];
+  };
+}
+
+const ProjectPage = async ({ params }: ProjectPageProps) => {
   const slug = Array.isArray(params.projectSlug)
     ? params.projectSlug[0]
     : params.projectSlug;
 
-  return isProductReleased ? (
-    <ProjectProvider slug={slug}>
-      <ProjectDetail />
-    </ProjectProvider>
-  ) : (
-    redirect(Routes.KycLanding)
-  );
+  if (!isProductReleased) {
+    redirect(Routes.KycLanding);
+  }
+
+  const initialData = await fetchProjectBySlug(slug);
+
+  if (!initialData) {
+    // Handle the case where the project is not found
+    redirect('/404');
+  }
+
+  return <ProjectView slug={slug} />;
 };
 
-export default ProjectRoute;
+export default ProjectPage;
