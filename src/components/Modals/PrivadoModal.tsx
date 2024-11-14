126407e29a3a76aa9a45b2b2ff2c310cded1e66f
Cherik <Pourcheriki@gmail.com>
2024-11-14 19:55:00 +0330

    separate generatePrivadoShortenedUrl

--- src/components/Modals/PrivadoModal.tsx
+++ src/components/Modals/PrivadoModal.tsx
@@ -1,23 +1,10 @@
 import { useEffect, useState, type FC } from 'react';
-// eslint-disable-next-line import/named
-import { v4 as uuidv4 } from 'uuid';
 import Modal, { BaseModalProps } from '../Modal';
 import { Button } from '../Button';
 import { IconArrowRight } from '../Icons/IconArrowRight';
-// eslint-disable-next-line import/named
-import config from '@/config/configuration';
-import { KYC_EXCLUDED_COUNTRIES } from '@/lib/constants/privado';
-import { generatePrivadoUuid } from '@/app/actions/generate-privado-uuid';
-interface PrivadoModalProps extends BaseModalProps {}
+import { generatePrivadoShortenedUrl } from '@/services/privado.service';
 
-const {
-  chain,
-  contractAddress,
-  chainName,
-  allowedIssuers,
-  webWalletBaseUrl,
-  verifierDid,
-} = config.privadoConfig;
+interface PrivadoModalProps extends BaseModalProps {}
 
 export const PrivadoModal: FC<PrivadoModalProps> = props => {
   const [isPrivadoLoading, setIsPrivadoLoading] = useState(false);
@@ -33,75 +20,12 @@ export const PrivadoModal: FC<PrivadoModalProps> = props => {
     const verifyAccount = async () => {
       try {
         setIsPrivadoLoading(true);
-        // Define the verification request
-        const baseUrl =
-          typeof window !== 'undefined' ? window.location.origin : '';
-        const excludedCountryCodes = Object.values(KYC_EXCLUDED_COUNTRIES).sort(
-          (a, b) => a - b,
-        );
-
-        const generatedUuid = uuidv4(); // Generate a unique UUID
-        const generatedThreadUuid = uuidv4(); // Generate another UUID for thid
-
-        const verificationRequest = {
-          id: generatedUuid,
-          typ: 'application/iden3comm-plain-json',
-          type: 'https://iden3-communication.io/proofs/1.0/contract-invoke-request',
-          thid: generatedThreadUuid,
-          from: verifierDid,
-          body: {
-            scope: [
-              {
-                circuitId: 'credentialAtomicQueryV3OnChain-beta.1',
-                id: config.privadoConfig.requestId,
-                query: {
-                  allowedIssuers: allowedIssuers,
-                  context:
-                    'https://raw.githubusercontent.com/anima-protocol/claims-polygonid/main/schemas/json-ld/poi-v2.json-ld',
-                  type: 'AnimaProofOfIdentity',
-                  credentialSubject: {
-                    document_country_code: {
-                      $nin: excludedCountryCodes,
-                    },
-                  },
-                },
-                params: {
-                  nullifierSessionId: config.privadoConfig.requestId.toString(),
-                },
-              },
-            ],
-            transaction_data: {
-              contract_address: contractAddress,
-              method_id: config.privadoConfig.method.methodId,
-              chain_id: chain.id,
-              network: chainName,
-            },
-          },
-        };
-
-        const shortenedUrlUuid = await generatePrivadoUuid(verificationRequest);
-        const shortenedUrl = encodeURIComponent(
-          `${baseUrl}/api/link-store?id=${shortenedUrlUuid}`,
-        );
-
-        console.log('verificationRequest', verificationRequest);
-
-        // Define the URLs for redirection
-        const backUrl = encodeURIComponent(`${baseUrl}/create/verify-privado`);
-        const finishUrl = encodeURIComponent(
-          `${baseUrl}/create/verify-privado`,
-        );
-
-        // Configure the Wallet URL (universal link)
-        const walletUrlWithMessage = `${webWalletBaseUrl}/#request_uri=${shortenedUrl}&back_url=${backUrl}&finish_url=${finishUrl}`;
-        setIsPrivadoLoading(false);
-
-        console.log('walletUrlWithMessage', walletUrlWithMessage);
-
-        setUrl(walletUrlWithMessage);
+        const url = await generatePrivadoShortenedUrl();
+        if (url) setUrl(url);
       } catch (error) {
+        console.error(error);
+      } finally {
         setIsPrivadoLoading(false);
-        console.error('Error in verifyAccount:', error);
       }
     };
     verifyAccount();
