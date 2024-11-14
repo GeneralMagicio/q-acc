67304bef80b322959af380af8b63605b2d5bb667
Cherik <Pourcheriki@gmail.com>
2024-11-14 20:13:24 +0330

    remove remaining time

--- src/components/QaccRoundCounter.tsx
+++ src/components/QaccRoundCounter.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import useRemainingTime from '@/hooks/useRemainingTime';
 import { usePrivado } from '@/hooks/usePrivado';
 
@@ -14,11 +13,11 @@ const QaccRoundCounter: React.FC<QaccRoundCounterProps> = ({ page }) => {
     '2024-11-25T12:00:00.000Z', // Currently set to start at 25th November
   );
   return (
-    <div className='px-10 py-6  bg-white rounded-2xl shadow-baseShadow font-redHatText z-40'>
+    <div className='px-10 py-6 rounded-2xl  font-redHatText z-40'>
       <div
-        className={`flex  flex-col gap-6 md:flex-row ${page === 'project' ? 'md:justify-center' : 'md:justify-between'} items-center`}
+        className={`flex  flex-col gap-6 md:flex-row justify-center items-center`}
       >
-        {page === 'project' ? (
+        {/* {page === 'project' ? (
           <div>
             <p className='text-[#1D1E1F] font-medium text-xl'>
               q/acc round starts in
@@ -26,17 +25,17 @@ const QaccRoundCounter: React.FC<QaccRoundCounterProps> = ({ page }) => {
           </div>
         ) : (
           ''
-        )}
+        )} */}
 
         <div>
-          <div className='px-6 py-4 shadow-baseShadow rounded-xl'>
+          {/* <div className='px-6 py-4 shadow-baseShadow rounded-xl'>
             <span className='text-[#5326EC] font-bold text-lg md:text-2xl '>
               {remainingTime}
             </span>
-          </div>
+          </div> */}
         </div>
 
-        {page === 'project' ? (
+        {/* {page === 'project' ? (
           ''
         ) : (
           <>
@@ -48,7 +47,7 @@ const QaccRoundCounter: React.FC<QaccRoundCounterProps> = ({ page }) => {
               </Link>
             )}
           </>
-        )}
+        )} */}
       </div>
     </div>
   );
