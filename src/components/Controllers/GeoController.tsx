22dc11c20098a23d870ddb2dd8f1fc8ee0624727
Cherik <Pourcheriki@gmail.com>
2024-11-20 12:38:15 +0330

    show modal for restricted countries

--- src/components/Controllers/GeoController.tsx
+++ src/components/Controllers/GeoController.tsx
@@ -1,14 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchGeoData } from '@/services/geo.service';
+import { RestrictModal } from '../Modals/RestrictModal';
+import { restrictedCountries } from '@/lib/constants/countries';
 
 export const GeoController = () => {
+  const [showRestrictModal, setShowRestrictModal] = useState(false);
   useEffect(() => {
     async function fetchUserLocation() {
-      const res = await fetchGeoData();
-      console.log('res', res);
+      try {
+        const locationInfo = await fetchGeoData();
+        if (locationInfo) {
+          if (restrictedCountries.includes(locationInfo.country)) {
+            setShowRestrictModal(true);
+          }
+        }
+      } catch (error: any) {
+        console.error('error on fetchUserLocation', error.message);
+      }
     }
 
     fetchUserLocation();
   }, []);
-  return <div>GeoController</div>;
+  return showRestrictModal ? (
+    <RestrictModal
+      isOpen={showRestrictModal}
+      onClose={() => setShowRestrictModal(false)}
+    />
+  ) : null;
 };
