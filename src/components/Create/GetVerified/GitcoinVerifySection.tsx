678acd401cdc22dda0aff7902855e1c7503e59e9
Cherik <Pourcheriki@gmail.com>
2024-11-28 01:51:20 +0330

    add statuses

--- src/components/Create/GetVerified/GitcoinVerifySection.tsx
+++ src/components/Create/GetVerified/GitcoinVerifySection.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import links from '@/lib/constants/links';
 import { Button, ButtonColor, ButtonStyle } from '@/components/Button';
+import { useFetchUser } from '@/hooks/useFetchUser';
+import { useFetchUserGitcoinPassportScore } from '@/hooks/userFetchUserGitcoinPassportScore';
+import config from '@/config/configuration';
+
+enum GitcoinVerificationStatus {
+  NOT_CHECKED,
+  ANALYSIS_PASS,
+  SCORER_PASS,
+  LOW_SCORE,
+}
 
 export const GitcoinVerifySection = () => {
+  const [status, setStatus] = useState(GitcoinVerificationStatus.NOT_CHECKED);
+  const { data: user, isLoading: isUserLoading, isSuccess } = useFetchUser();
+  const { refetch: refetchScore, isLoading: isLoadingScore } =
+    useFetchUserGitcoinPassportScore();
+
+  useEffect(() => {
+    if (!isSuccess) return;
+    if ((user?.analysisScore || 0) > config.GP_ANALYSIS_SCORE_THRESHOLD) {
+      setStatus(GitcoinVerificationStatus.ANALYSIS_PASS);
+    }
+  }, [isSuccess, user?.analysisScore, user?.passportScore]);
+
   return (
     <section className='bg-gray-100 rounded-2xl p-6 flex flex-col gap-4'>
       <h1 className='text-lg font-bold'>Gitcoin Passport</h1>
@@ -21,6 +43,7 @@ export const GitcoinVerifySection = () => {
         styleType={ButtonStyle.Solid}
         color={ButtonColor.Pink}
         className='mr-auto px-16'
+        loading={isUserLoading}
       >
         Check Score
       </Button>
