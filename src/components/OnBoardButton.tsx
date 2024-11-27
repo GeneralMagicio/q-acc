eb425b2cb57cc1102a5e5d9ed45f848d6a5c05aa
Cherik <Pourcheriki@gmail.com>
2024-11-27 23:45:51 +0330

    handle connect wallet

--- src/components/OnBoardButton.tsx
+++ src/components/OnBoardButton.tsx
@@ -1,5 +1,7 @@
 import React, { HTMLAttributes } from 'react';
 import Link from 'next/link';
+import { useAccount } from 'wagmi';
+import { useWeb3Modal } from '@web3modal/wagmi/react';
 import { usePrivado } from '@/hooks/usePrivado';
 import Routes from '@/lib/constants/Routes';
 import { useFetchUser } from '@/hooks/useFetchUser';
@@ -8,9 +10,16 @@ export const customButtonClass: HTMLAttributes<HTMLDivElement>['className'] =
   'font-redHatText px-10 py-6 flex justify-center items-center text-[white] font-bold bg-pink-500 rounded-full shadow-tabShadow text-sm';
 
 export const OnBoardButton = () => {
+  const { address } = useAccount();
   const { isVerified } = usePrivado();
   const { data: user } = useFetchUser();
-  return isVerified ? (
+  const { open } = useWeb3Modal();
+
+  return !address ? (
+    <div className={customButtonClass} onClick={() => open()}>
+      q/acc Sign in
+    </div>
+  ) : isVerified ? (
     <Link href={Routes.Projects}>
       <div className={customButtonClass}>View Projects</div>
     </Link>
