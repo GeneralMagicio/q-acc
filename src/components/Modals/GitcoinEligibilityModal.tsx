f7e4786c501e1e220e5ea5e46163e1b0a64879de
Cherik <Pourcheriki@gmail.com>
2024-11-28 03:17:16 +0330

    update modal

--- src/components/Modals/GitcoinEligibilityModal.tsx
+++ src/components/Modals/GitcoinEligibilityModal.tsx
@@ -1,30 +1,21 @@
-import { useState, type FC } from 'react';
+import { type FC } from 'react';
 import Modal, { BaseModalProps } from '../Modal';
 import { Button, ButtonColor, ButtonStyle } from '../Button';
 import { EligibilityBadge, EligibilityBadgeStatus } from '../EligibilityBadge';
-import { IconInfoCircle } from '../Icons/IconInfoCircle';
-import { IconGitcoin } from '../Icons/IconGitcoin';
-import config from '@/config/configuration';
-import { useFetchUserGitcoinPassportScore } from '@/hooks/userFetchUserGitcoinPassportScore';
-import { useFetchUser } from '@/hooks/useFetchUser';
+
+import {
+  GitcoinVerificationStatus,
+  useGitcoinScore,
+} from '@/hooks/useGitcoinScore';
+import { GitcoinPassLow } from '../GitcoinVerifcationElements/GitcoinPassLow';
 
 interface GitcoinEligibilityModalProps extends BaseModalProps {}
-
-enum GitcoinEligibilityModalState {
-  CHECK,
-  NOT_CONNECTED,
-  ELIGIBLE,
-  NOT_ELIGIBLE,
-}
 
 export const GitcoinEligibilityModal: FC<
   GitcoinEligibilityModalProps
 > = props => {
-  const [state, setState] = useState(GitcoinEligibilityModalState.CHECK);
-  const [userGitcoinScore, setUserGitcoinScore] = useState(0);
-
-  const { data: user } = useFetchUser();
-  const { refetch, isFetching } = useFetchUserGitcoinPassportScore();
+  const { status, userGitcoinScore, onCheckScore, isScoreFetching } =
+    useGitcoinScore();
 
   return (
     <Modal {...props} title='Eligibility Check' showCloseButton>
@@ -33,94 +24,31 @@ export const GitcoinEligibilityModal: FC<
           Verify your eligibility to contribute up to $1,000 to this project by
           completing a quick on-chain activity check.
         </p>
-        {state === GitcoinEligibilityModalState.CHECK && (
+        {status === GitcoinVerificationStatus.NOT_CHECKED && (
           <Button
             className='mx-auto'
             styleType={ButtonStyle.Solid}
             color={ButtonColor.Pink}
-            loading={isFetching}
+            loading={isScoreFetching}
             onClick={async () => {
-              let _score;
-              if (user?.analysisScore) {
-                _score = user.analysisScore;
-              } else {
-                const res = await refetch();
-                _score = res.data?.analysisScore;
-              }
-              setUserGitcoinScore(_score || 0);
-              if (_score !== undefined) {
-                setState(
-                  _score > config.GP_ANALYSIS_SCORE_THRESHOLD
-                    ? GitcoinEligibilityModalState.ELIGIBLE
-                    : GitcoinEligibilityModalState.NOT_ELIGIBLE,
-                );
-              } else {
-                setState(GitcoinEligibilityModalState.NOT_CONNECTED);
-              }
+              await onCheckScore();
             }}
           >
-            Check Eligibility
+            Check Score
           </Button>
         )}
-        {state === GitcoinEligibilityModalState.NOT_CONNECTED && (
-          <div className='flex flex-col'>
-            <EligibilityBadge
-              status={EligibilityBadgeStatus.NOT_ELIGIBLE}
-              className='block ml-auto'
-            />
-            <div className='bg-gray-50 my-2 rounded-xl px-4 py-6 text-base'>
-              You didn’t pass the check. Please connect your Gitcoin Passport.
-            </div>
-            <Button
-              className='mx-auto'
-              styleType={ButtonStyle.Solid}
-              color={ButtonColor.Pink}
-            >
-              Connect Gitcoin Passport
-            </Button>
-          </div>
+        {status === GitcoinVerificationStatus.ANALYSIS_PASS && (
+          <EligibilityBadge
+            className='ml-auto w-fit'
+            status={EligibilityBadgeStatus.ELIGIBLE}
+          />
         )}
-        {(state === GitcoinEligibilityModalState.ELIGIBLE ||
-          state === GitcoinEligibilityModalState.NOT_ELIGIBLE) && (
-          <div className='flex flex-col'>
-            <EligibilityBadge
-              status={
-                userGitcoinScore > config.GP_ANALYSIS_SCORE_THRESHOLD
-                  ? EligibilityBadgeStatus.ELIGIBLE
-                  : EligibilityBadgeStatus.NOT_ELIGIBLE
-              }
-              className='block ml-auto'
-            />
-            <div className='my-2 rounded-xl p-4 text-base border-[1px] border-gray-200'>
-              <div className='flex gap-2 text-gray-500'>
-                <IconInfoCircle size={24} />
-                <div>Required Passport score to be eligible</div>
-                <div>
-                  {' >'}
-                  {config.GP_ANALYSIS_SCORE_THRESHOLD}
-                </div>
-              </div>
-              <div className='bg-gray-50 mt-2 rounded-xl p-4 text-base text-black flex items-center justify-between'>
-                Your Passport Score
-                <div className='bg-black text-white py-2 px-6 rounded-full'>
-                  {userGitcoinScore}
-                </div>
-              </div>
-            </div>
-            <div className='flex justify-between items-center'>
-              <div className='text-gray-800'>Passport connected</div>
-              <Button
-                styleType={ButtonStyle.Text}
-                color={ButtonColor.Pink}
-                className='shadow-GIV400'
-              >
-                <div className='flex gap-2'>
-                  <IconGitcoin size={16} />
-                  Refresh Score
-                </div>
-              </Button>
-            </div>
-          </div>
+        {(status === GitcoinVerificationStatus.SCORER_PASS ||
+          status === GitcoinVerificationStatus.LOW_SCORE) && (
+          <GitcoinPassLow
+            onCheckScore={onCheckScore}
+            userGitcoinScore={userGitcoinScore}
+          />
         )}
       </div>
     </Modal>
