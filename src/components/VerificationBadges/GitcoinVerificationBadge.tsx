3ee77b089aa19d17de0cae4bc5a6276bd6044098
ali <aliebrahimi2079@gmail.com>
2024-11-29 00:11:39 +0330

    redirect to verification tab

--- src/components/VerificationBadges/GitcoinVerificationBadge.tsx
+++ src/components/VerificationBadges/GitcoinVerificationBadge.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { IconGitcoinPassport } from '../Icons/IconGitcoinPassport';
 import {
   GitcoinVerificationStatus,
@@ -11,12 +12,20 @@ export const GitcoinVerificationBadge = () => {
     status === GitcoinVerificationStatus.ANALYSIS_PASS ||
     status === GitcoinVerificationStatus.SCORER_PASS;
 
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`${router.pathname}?tab=verification`);
+  };
+
   return (
     <div
       className={`flex gap-1 ${isVerified ? 'bg-teal-500' : 'bg-amber-500'} text-white py-1 px-2 rounded-lg`}
     >
       <IconGitcoinPassport size={24} />
-      <span>Gitcoin Passport {isVerified ? 'Verified' : 'Not Verified'}</span>
+      <span onClick={handleClick} className='cursor-pointer'>
+        Gitcoin Passport {isVerified ? 'Verified' : 'Not Verified'}
+      </span>
     </div>
   );
 };
--- src/components/VerificationBadges/PrivadoVerificationBadge.tsx
+++ src/components/VerificationBadges/PrivadoVerificationBadge.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-
+import { useRouter } from 'next/router';
 import { usePrivado } from '@/hooks/usePrivado';
 import { IconPrivado } from '../Icons/IconPrivado';
 
 export const PrivadoVerificationBadge = () => {
   const { isVerified } = usePrivado();
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`${router.pathname}?tab=verification`);
+  };
 
   return (
     <div
       className={`flex gap-1 ${isVerified ? 'bg-teal-500' : 'bg-amber-500'} text-white py-1 px-2 rounded-lg`}
     >
       <IconPrivado size={24} />
-      <span>Privado zkID {isVerified ? 'Verified' : 'Not Verified'}</span>
+      <span onClick={handleClick} className='cursor-pointer'>
+        Privado zkID {isVerified ? 'Verified' : 'Not Verified'}
+      </span>
     </div>
   );
 };
