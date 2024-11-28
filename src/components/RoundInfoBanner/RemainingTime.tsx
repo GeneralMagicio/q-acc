87262713135bec714e09e0309cb5daa482270f17
Cherik <Pourcheriki@gmail.com>
2024-11-29 01:38:07 +0330

    fix useRemainingTime

--- src/components/RoundInfoBanner/RemainingTime.tsx
+++ src/components/RoundInfoBanner/RemainingTime.tsx
@@ -10,7 +10,7 @@ export const RemainingTime: FC<IRenamingTimeProps> = ({
   startDate,
   endDate,
 }) => {
-  const remainingTime = useRemainingTime(startDate, endDate);
+  const remainingTime = useRemainingTime(startDate);
   return (
     <div className='text-giv-500 font-medium py-6 px-10 shadow-GIV400 rounded-2xl'>
       <div className='text-lg'>Remaining time</div>
