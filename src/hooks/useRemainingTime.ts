87b2e599d8308508712fa6b626ea2f99b65ce799
Cherik <Pourcheriki@gmail.com>
2024-11-29 01:37:57 +0330

    update useRemainingTime

--- src/hooks/useRemainingTime.ts
+++ src/hooks/useRemainingTime.ts
@@ -2,34 +2,27 @@
 import { useState, useEffect } from 'react';
 import { calculateRemainingTime } from '@/helpers/date';
 
-function useRemainingTime(startDate?: string, endDate?: string) {
+function useRemainingTime(date?: string) {
   const [remainingTime, setRemainingTime] = useState<string>('');
 
   useEffect(() => {
-    if (!startDate || !endDate) return;
-    const _startDate = new Date(startDate);
-    const _endDate = new Date(endDate);
+    if (!date) return;
+    const _date = new Date(date);
     const now = new Date();
 
-    // If current time is before the start date, don't display remaining time
-    if (now < _startDate) {
-      setRemainingTime('--:--:--');
-      return;
-    }
-
     // Set initial remaining time
-    setRemainingTime(calculateRemainingTime(_endDate));
+    setRemainingTime(calculateRemainingTime(_date));
 
     // Update remaining time every second
     const interval = setInterval(() => {
-      setRemainingTime(calculateRemainingTime(_endDate));
+      setRemainingTime(calculateRemainingTime(_date));
     }, 1000);
 
     // Cleanup interval on component unmount
     return () => clearInterval(interval);
-  }, [endDate]);
+  }, [date]);
 
-  return endDate ? remainingTime : '--:--:--';
+  return date ? remainingTime : '--:--:--';
 }
 
 export default useRemainingTime;
