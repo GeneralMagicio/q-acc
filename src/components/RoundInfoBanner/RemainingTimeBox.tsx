0795ae35de7ae059f8fc0f2de805ea2c29afd302
Cherik <Pourcheriki@gmail.com>
2024-11-29 01:49:57 +0330

    update RemainingTimeBox

--- src/components/RoundInfoBanner/RemainingTimeBox.tsx
+++ src/components/RoundInfoBanner/RemainingTimeBox.tsx
@@ -1,4 +1,4 @@
-import useRemainingTime from '@/hooks/useRemainingTime';
+import useTimeInterval from '@/hooks/useTimeInterval';
 import type { FC } from 'react';
 
 interface IRenamingTimeProps {
@@ -6,7 +6,7 @@ interface IRenamingTimeProps {
 }
 
 export const RemainingTimeBox: FC<IRenamingTimeProps> = ({ targetDate }) => {
-  const time = useRemainingTime(targetDate);
+  const time = useTimeInterval(targetDate);
   return (
     <div className='py-6 px-10 rounded-2xl shadow-xl shadow-giv-50 text-3xl font-bold text-giv-500'>
       {time}
