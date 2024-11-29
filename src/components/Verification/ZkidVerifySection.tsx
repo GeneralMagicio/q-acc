6dd518c2db56729e7b225bf71d6e5b84fd751c20
Cherik <Pourcheriki@gmail.com>
2024-11-29 18:15:42 +0330

    add privado success

--- src/components/Verification/ZkidVerifySection.tsx
+++ src/components/Verification/ZkidVerifySection.tsx
@@ -14,7 +14,13 @@ export const ZkidVerifySection = () => {
   const { isVerified, error, isLoading } = usePrivado();
   return isVerified ? (
     <section className='bg-gray-50 rounded-2xl p-6 flex gap-4 justify-between'>
-      <h1 className='text-lg font-bold'>Privado zkID</h1>
+      <div>
+        <h1 className='text-lg font-bold'>Privado zkID</h1>
+        <p>
+          You are eligible to support each project with up to{' '}
+          {config.HIGH_CAP_TEXT}.
+        </p>
+      </div>
       <EligibilityBadge status={EligibilityBadgeStatus.ELIGIBLE} />
     </section>
   ) : (
