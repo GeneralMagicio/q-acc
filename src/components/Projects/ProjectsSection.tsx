07cf1fe6cb90b62a1ebb9e50a0790ec482216f6a
Cherik <Pourcheriki@gmail.com>
2024-11-29 17:45:28 +0330

    separate projects section

---
+++ src/components/Projects/ProjectsSection.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useFetchAllProjects } from '@/hooks/useFetchAllProjects';
+import { Spinner } from '../Loading/Spinner';
+import { ProjectHoverCard } from '../ProjectCard/ProjectHoverCard';
+
+export const ProjectsSection = () => {
+  const { data: allProjects, isLoading } = useFetchAllProjects();
+  return isLoading ? (
+    <div className='min-h-80 flex items-center justify-center '>
+      <Spinner />
+    </div>
+  ) : (
+    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mb-10'>
+      {allProjects?.projects?.map(project => (
+        <ProjectHoverCard key={project.id} project={project} />
+      ))}
+    </div>
+  );
+};
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,18 +1,13 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { ProjectsBanner } from './ProjectsBanner';
-import { useFetchAllProjects } from '@/hooks/useFetchAllProjects';
-import { ProjectHoverCard } from '../ProjectCard/ProjectHoverCard';
 import RoundCountBanner from '../RoundCountBanner';
 import { useFetchActiveRoundDetails } from '@/hooks/useFetchActiveRoundDetails';
 import { getMostRecentEndRound } from '@/helpers/round';
 import { isEarlyAccessBranch } from '@/config/configuration';
-import { Spinner } from '../Loading/Spinner';
-
-const projectCardStyle = '';
+import { ProjectsSection } from './ProjectsSection';
 
 export const ProjectsView = () => {
-  const { data: allProjects, isLoading } = useFetchAllProjects();
   const { data: activeRoundDetails } = useFetchActiveRoundDetails();
   const [isRoundEnded, setIsRoundEnded] = useState(false);
 
@@ -33,31 +28,11 @@ export const ProjectsView = () => {
   return (
     <>
       <ProjectsBanner />
-      <div className='container mx-auto'>
+      <div className='container'>
         <div className='my-[60px]'>
-          {isEarlyAccessBranch
-            ? !isRoundEnded && <RoundCountBanner />
-            : // <QaccRoundCounter page='project' />
-              null}
+          {isEarlyAccessBranch ? !isRoundEnded && <RoundCountBanner /> : null}
         </div>
-
-        {isLoading ? (
-          <div className='min-h-80 flex items-center justify-center '>
-            <Spinner />
-          </div>
-        ) : (
-          <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mb-10'>
-            {allProjects?.projects?.map(project => (
-              // <Link href={`/project/${project.slug}`} key={project.id}>
-              <ProjectHoverCard
-                key={project.id}
-                project={project}
-                className={projectCardStyle}
-              />
-              // </Link>
-            ))}
-          </div>
-        )}
+        <ProjectsSection />
       </div>
     </>
   );
