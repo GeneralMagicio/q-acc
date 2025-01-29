9f66be932cd07700ecdce7eed881fac33c54abe2
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2025-01-29 16:15:13 -0500

    fix lint

--- src/components/ProjectCard/ProjectHoverCard.tsx
+++ src/components/ProjectCard/ProjectHoverCard.tsx
@@ -356,7 +356,7 @@ export const ProjectHoverCard: FC<ProjectCardProps> = ({
                     <Button
                       onClick={e => {
                         e.stopPropagation();
-                        const url = `https://quickswap.exchange/#/swap?outputCurrency=${project?.abc?.issuanceTokenAddress}`;
+                        const url = `https://quickswap.exchange/#/swap?currency0=${config.ERC_TOKEN_ADDRESS}&currency1=${project?.abc?.issuanceTokenAddress}`;
                         window.open(url, '_blank', 'noopener,noreferrer');
                       }}
                       color={ButtonColor.Pink}
