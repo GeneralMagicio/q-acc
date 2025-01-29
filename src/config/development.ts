ee677dce63886942c05847183446b3d9121f9802
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2025-01-29 16:15:13 -0500

    fix lint

--- src/config/development.ts
+++ src/config/development.ts
@@ -1,38 +1,38 @@
-import { polygonZkEvm } from 'wagmi/chains';
-
+import { polygonZkEvm, polygonZkEvmCardona } from 'wagmi/chains';
 import { EnvConfig } from '@/types/config.type';
-import { PrivadoContractMethodV2 } from '@/lib/constants/privado';
+import { PrivadoContractMethodV1 } from '@/lib/constants/privado';
 
 const config: EnvConfig = {
   SUPPORTED_CHAINS: [polygonZkEvm] as const,
+  ERC_TOKEN_ADDRESS: '0x961bB3932A7efAa9aDcc7409e1fea090479E8312',
+  ERC_TOKEN_SYMBOL: 'TPOL',
   SCAN_URL: 'https://zkevm.polygonscan.com/',
-  ERC_TOKEN_ADDRESS: '0x22B21BedDef74FE62F031D2c5c8F7a9F8a4b304D',
-  ERC_TOKEN_SYMBOL: 'POL',
   GRAPHQL_ENDPOINT:
     process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
-    'https://qacc-be.generalmagic.io/graphql',
+    'https://staging.qacc-be.generalmagic.io/graphql',
   AUTH_BASE_ROUTE:
-    process.env.NEXT_PUBLIC_AUTH_BASE_ROUTE || 'https://auth.giveth.io/v1',
-  GIVETH_GQL_ENDPOINT: 'https://mainnet.serve.giveth.io/graphql',
-  NETWORK_RPC_ADDRESS: 'https://zkevm-rpc.com',
+    process.env.NEXT_PUBLIC_AUTH_BASE_ROUTE ||
+    'https://auth.serve.giveth.io/v1',
+  GIVETH_GQL_ENDPOINT: 'https://impact-graph.serve.giveth.io/graphql',
+  NETWORK_RPC_ADDRESS: 'https://zkevm-rpc.com	',
   INDEXER_GRAPHQL_URL: 'https://indexer.hyperindex.xyz/a414bf3/v1/graphql',
   privadoConfig: {
-    chain: polygonZkEvm,
-    chainName: 'zkevm',
-    contractAddress: '0xfcc86A79fCb057A8e55C6B853dff9479C3cf607c',
+    chain: polygonZkEvmCardona,
+    chainName: 'zkevm_cardona',
     allowedIssuers: [
-      'did:iden3:privado:main:2ScrbEuw9jLXMapW3DELXBbDco5EURzJZRN1tYj7L7',
+      'did:iden3:privado:main:2SdUfDwHK3koyaH5WzhvPhpcjFfdem2xD625aymTNc',
     ],
-    requestId: 12,
-    webWalletBaseUrl: 'https://wallet.privado.id',
+    contractAddress: '0xdE9eBC446d69EF9a876a377e3E3cEe91d08fE2A0',
+    requestId: 34,
+    webWalletBaseUrl: 'https://wallet-dev.privado.id',
     verifierDid:
-      'did:iden3:polygon:amoy:x6x5sor7zpyefHwZu9RE4xiuRWBkq9xAEHxrKbKWb',
-    method: PrivadoContractMethodV2,
+      'did:iden3:polygon:amoy:x6x5sor7zpyWUUVJNZLzuDgMmeZfR2thKN2uMui8J',
+    method: PrivadoContractMethodV1,
   },
   GP_ANALYSIS_SCORE_THRESHOLD: 50,
   GP_SCORER_SCORE_THRESHOLD: 15,
   MINIMUM_DONATION_AMOUNT: 20,
-  MATCHING_FUND_ADDRESS: [],
+  MATCHING_FUND_ADDRESS: ['0x52ff9be3a6758f5d3b2f2949c7e9c95563d53777'],
 };
 
 export default config;
