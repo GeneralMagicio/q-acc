9a410a11759576f56d3ed2cd08043c02c0a48350
Amin Latifi <a.latifi.al@gmail.com>
2025-03-31 13:49:01 +0330

    Update production config to use development wallet URL and adjust request ID to amoy one

--- src/config/production.ts
+++ src/config/production.ts
@@ -24,8 +24,8 @@ const config: EnvConfig = {
     allowedIssuers: [
       'did:iden3:privado:main:2ScrbEuw9jLXMapW3DELXBbDco5EURzJZRN1tYj7L7',
     ],
-    requestId: 12,
-    webWalletBaseUrl: 'https://wallet.privado.id',
+    requestId: 10,
+    webWalletBaseUrl: 'https://wallet-dev.privado.id',
     verifierDid:
       'did:iden3:polygon:amoy:x6x5sor7zpyefHwZu9RE4xiuRWBkq9xAEHxrKbKWb',
     method: PrivadoContractMethodV2,
