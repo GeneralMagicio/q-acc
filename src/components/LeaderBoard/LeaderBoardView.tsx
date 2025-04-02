48373505df3f3163e91d5fec4fd28c061b5707b7
Cherik <Pourcheriki@gmail.com>
2025-04-02 11:22:02 +0330

    init UserInfo

--- src/components/LeaderBoard/LeaderBoardView.tsx
+++ src/components/LeaderBoard/LeaderBoardView.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Banner } from './Banner';
+import { UserInfo } from './UserInfo';
 
 export const LeaderBoardView = () => {
   return (
     <div className='container'>
       <Banner />
+      <div className='bg-white rounded-xl p-6'>
+        <UserInfo />
+      </div>
     </div>
   );
 };
