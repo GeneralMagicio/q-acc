7ac3a40bbfdba5cb14a1e8a013aa05d6ce4dce7f
Cherik <Pourcheriki@gmail.com>
2025-04-03 14:05:28 +0330

    show real data

--- src/components/LeaderBoard/LeaderBoardView.tsx
+++ src/components/LeaderBoard/LeaderBoardView.tsx
@@ -2,90 +2,21 @@ import React from 'react';
 import Image from 'next/image';
 import { Banner } from './Banner';
 import { UserInfo } from './UserInfo';
+import { useFetchLeaderBoard } from '@/hooks/useFetchLeaderBoard';
 
 const tableHeaders = [
-  { name: 'Rank', isSortable: true },
-  { name: 'Supporter', isSortable: false },
-  { name: 'q/acc points', isSortable: true },
-  { name: 'Projects funded', isSortable: true },
-];
-
-const mockData = [
-  {
-    rank: 1,
-    name: 'John Doe',
-    points: 392,
-    projects: 2,
-  },
-  {
-    rank: 2,
-    name: 'Jane Smith',
-    points: 300,
-    projects: 3,
-  },
-  {
-    rank: 3,
-    name: 'Alice Johnson',
-    points: 250,
-    projects: 4,
-  },
-  {
-    rank: 4,
-    name: 'Bob Brown',
-    points: 200,
-    projects: 5,
-  },
-  {
-    rank: 5,
-    name: 'Charlie Davis',
-    points: 150,
-    projects: 6,
-  },
-  {
-    rank: 6,
-    name: 'David Wilson',
-    points: 100,
-    projects: 7,
-  },
-  {
-    rank: 7,
-    name: 'Eva Green',
-    points: 50,
-    projects: 8,
-  },
-  {
-    rank: 8,
-    name: 'Frank White',
-    points: 25,
-    projects: 9,
-  },
-  {
-    rank: 9,
-    name: 'Grace Black',
-    points: 10,
-    projects: 10,
-  },
-  {
-    rank: 10,
-    name: 'Hannah Blue',
-    points: 5,
-    projects: 11,
-  },
-  {
-    rank: 11,
-    name: 'Ian Red',
-    points: 3,
-    projects: 12,
-  },
-  {
-    rank: 12,
-    name: 'Jack Yellow',
-    points: 2,
-    projects: 13,
-  },
+  { name: 'Rank', sortField: null },
+  { name: 'Supporter', sortField: null },
+  { name: 'q/acc points', sortField: 'QaccPoints' },
+  { name: 'Projects funded', sortField: 'ProjectsFundedCount' },
 ];
 
 export const LeaderBoardView = () => {
+  const { data: leaderboardInfo } = useFetchLeaderBoard(10, 0, {
+    field: 'QaccPoints',
+    direction: 'DESC',
+  });
+  console.log('leaderboardInfo', leaderboardInfo);
   return (
     <div className='container'>
       <Banner />
@@ -99,7 +30,7 @@ export const LeaderBoardView = () => {
             {tableHeaders.map((header, index) => (
               <div key={index} className='font-bold flex gap-1'>
                 {header.name}
-                {header.isSortable && (
+                {header.sortField && (
                   <Image
                     src='/images/icons/sort.svg'
                     alt='sort'
@@ -114,14 +45,15 @@ export const LeaderBoardView = () => {
               </div>
             ))}
           </div>
-          {mockData.map(user => {
-            const isTop = user.rank <= 3;
+          {leaderboardInfo?.users.map((user, idx) => {
+            const rank = idx + 1;
+            const isTop = rank <= 3;
             return (
               <div
-                key={user.rank}
+                key={rank}
                 className={`grid grid-cols-[50px_1fr_150px_150px] gap-4 text-base py-4 text-gray-700 font-redHatText border-t-[1px] border-gray-200 ${isTop ? 'bg-giv-50' : ''} hover:bg-gray-50 transition duration-200 ease-in-out`}
               >
-                <div className='text-right'>#{user.rank}</div>
+                <div className='text-right'>#{rank}</div>
                 <div className='flex gap-2'>
                   {isTop && (
                     <Image
@@ -134,8 +66,8 @@ export const LeaderBoardView = () => {
                   )}
                   {user.name}
                 </div>
-                <div className=''>{user.points}</div>
-                <div className=''>{user.projects}</div>
+                <div className=''>{user.qaccPoints}</div>
+                <div className=''>{user.projectsFundedCount}</div>
               </div>
             );
           })}
