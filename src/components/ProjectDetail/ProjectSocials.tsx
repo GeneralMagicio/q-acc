812322fa324f1938ccbeca6e3944e89ad6d0b515
Cherik <Pourcheriki@gmail.com>
2025-04-07 13:06:34 +0330

    update project socials

--- src/components/ProjectDetail/ProjectSocials.tsx
+++ src/components/ProjectDetail/ProjectSocials.tsx
@@ -13,19 +13,19 @@ import { IconTelegram } from '../Icons/IconTelegram';
 import { IconGithub } from '../Icons/IconGithub';
 import { useProjectContext } from '@/context/project.context';
 
-const iconMap: { [key: string]: React.ReactNode } = {
+export const socialMediaIconMap: { [key: string]: React.ReactNode } = {
   facebook: <IconFacebook color='#4267B2' size={16} />,
-  x: <IconXSocial color={'#26A7DE'} size={16} />,
-  instagram: <IconInstagram color={'#8668FC'} size={16} />,
-  youtube: <IconYoutube color={'#C4302B'} size={16} />,
-  linkedin: <IconLinkedin color={'#165FFA'} size={16} />,
-  reddit: <IconReddit color={'#FF5700'} size={16} />,
-  discord: <IconDiscord color={'#7289DA'} size={16} />,
-  website: <IconWebsite color={'#2EA096'} size={16} />,
-  telegram: <IconTelegram color={'#229ED9'} size={16} />,
-  github: <IconGithub color={'#1D1E1F'} size={16} />,
+  x: <IconXSocial size={16} />,
+  instagram: <IconInstagram size={16} />,
+  youtube: <IconYoutube size={16} />,
+  linkedin: <IconLinkedin size={16} />,
+  reddit: <IconReddit size={16} />,
+  discord: <IconDiscord size={16} />,
+  website: <IconWebsite size={16} />,
+  telegram: <IconTelegram size={16} />,
+  github: <IconGithub size={16} />,
 };
-const socialMediaColor: { [key: string]: string } = {
+export const socialMediaColor: { [key: string]: string } = {
   facebook: '#4267B2',
   x: '#26A7DE',
   instagram: '#8668FC',
@@ -37,12 +37,13 @@ const socialMediaColor: { [key: string]: string } = {
   telegram: '#229ED9',
   github: '#1D1E1F',
 };
+
+export const removeHttpsAndWwwFromUrl = (socialMediaUrl: string) => {
+  return socialMediaUrl.replace('https://', '').replace('www.', '');
+};
+
 const ProjectSocials = () => {
   const { projectData } = useProjectContext();
-
-  const removeHttpsAndWwwFromUrl = (socialMediaUrl: string) => {
-    return socialMediaUrl.replace('https://', '').replace('www.', '');
-  };
 
   return (
     <div className='container  max-w-[800px] '>
@@ -50,11 +51,10 @@ const ProjectSocials = () => {
         Find us on our social media
       </h1>
       <br />
-      <div className='flex flex-wrap gap-6'>
-        {/* {projectData?.socialMedia?.map((social: IProjectSocialMedia) => ( */}
+      <div className='flex flex-wrap gap-2'>
         {projectData?.socialMedia?.map((social: any) => {
           const color = socialMediaColor[social.type.toLowerCase()];
-          const icon = iconMap[social.type.toLowerCase()];
+          const icon = socialMediaIconMap[social.type.toLowerCase()];
           return (
             <div
               key={social.link}
