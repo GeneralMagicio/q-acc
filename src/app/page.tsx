95bbadeb7466331b0dd8ce5e1f6fe77b1239e85f
Cherik <Pourcheriki@gmail.com>
2025-04-08 19:42:28 +0330

    init first part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
+import { Inter } from 'next/font/google';
 import { Banner } from '@/components/Banner/Banner';
 import Collaborator from '@/components/Collaborator';
 import { isProductReleased } from '@/config/configuration';
@@ -11,10 +12,9 @@ import { OnBoardButton } from '@/components/OnBoardButton';
 import { Support } from '@/components/Support';
 import { QaccProjectsCard } from '@/components/QaccProjectsCard';
 
-export default function Home() {
-  // const { data: activeRoundDetails, isLoading } = useFetchActiveRoundDetails();
+const inter = Inter({ subsets: ['latin'] });
 
-  // const isQaccRoundEnded = useFetchMostRecentEndRound(activeRoundDetails);
+export default function Home() {
   return isProductReleased ? (
     <main className='flex flex-col '>
       <Banner />
@@ -24,37 +24,48 @@ export default function Home() {
           alt='round'
           width={500}
           height={500}
-          style={{ position: 'absolute', top: '0', right: '0', opacity: 0.3 }}
+          style={{
+            position: 'absolute',
+            top: '0',
+            right: '0',
+            opacity: 0.3,
+            zIndex: 0,
+          }}
         />
-        <div className='container flex flex-col gap-10 pt-4 pb-20 font-light text-2xl text-gray-600'>
-          {/* Qacc Banner Stats */}
-          {/* {isLoading ? (
-            ''
-          ) : !activeRoundDetails ? (
-            <QaccRoundEndBanner />
-          ) : (
-            <RoundStatusBanner />
-          )} */}
-
-          <div className='flex flex-col gap-6 mx-auto w-[80%]'>
-            <h1 className='text-4xl text-gray-900 font-bold mt-10'>
-              Quadratic Acceleration (q/acc): The Future of Trust in
-              Tokenization
-            </h1>
-            <div className='leading-9 text-[#4F576A]'>
-              <p>
-                Web3 is the future, but let’s be honest—it’s chaotic. Scams, rug
-                pulls, and hype-driven speculation make it nearly impossible for
-                many to separate real innovation from noise; q/acc changes that.
-              </p>
-              <br />
-              <p>
-                The most promising Web3 startups are hand-picked to launch their
-                tokens in a way that’s secure, transparent, and rug-proof. No
-                more guessing games. No more losing to bad actors. Just pure,
-                accelerated innovation
-              </p>
-              <br />
+        <div className='container relative flex flex-col gap-10 pt-4 pb-20 font-light text-2xl text-gray-600'>
+          <div className='flex flex-col gap-6 '>
+            <div className='grid grid-cols-2 gap-4'>
+              <div>
+                <h1
+                  className={`text-3xl text-gray-900 font-bold mt-10 ${inter.className} mb-6`}
+                >
+                  Quadratic Acceleration (q/acc): The Future of Trust in
+                  Tokenization
+                </h1>
+                <div className='text-2xl leading-9 text-gray-600'>
+                  <p>
+                    Web3 is the future, but let’s be honest—it’s chaotic. Scams,
+                    rug pulls, and hype-driven speculation make it nearly
+                    impossible for many to separate real innovation from noise;
+                    q/acc changes that.
+                  </p>
+                  <br />
+                  <p>
+                    The most promising Web3 startups are hand-picked to launch
+                    their tokens in a way that’s secure, transparent, and
+                    rug-proof. No more guessing games. No more losing to bad
+                    actors. Just pure, accelerated innovation
+                  </p>
+                  <br />
+                </div>
+              </div>
+              <iframe
+                className='w-full rounded-lg h-full my-6'
+                src='https://www.youtube.com/embed/m30ElzaR--4'
+                title='Quadratic Acceleration (q/acc): The Future of Tokenization'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                allowFullScreen={true}
+              ></iframe>
             </div>
           </div>
         </div>
