d8c963a5cee1fc2e27109e9e049bd64354683880
Cherik <Pourcheriki@gmail.com>
2025-04-08 21:17:11 +0330

    remove the gap

--- src/components/QaccProjectsCard.tsx
+++ src/components/QaccProjectsCard.tsx
@@ -6,7 +6,7 @@ import { ProjectsFilter } from './ProjectsFilter';
 export const QaccProjectsCard = () => {
   const [seasonFilter, setSeasonFilter] = useState(0);
   return (
-    <div className='bg-white relative pt-8 pb-5 mt-5'>
+    <div className='bg-white relative pt-8 pb-5'>
       <div className='container '>
         <ProjectsFilter
           seasonFilter={seasonFilter}
