13d5581e4b81648084bd15cea428b2a3a2194ca9
Cherik <Pourcheriki@gmail.com>
2025-04-09 11:42:46 +0330

    update projects page

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,15 +1,23 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { ProjectsSection } from './ProjectsSection';
 import { Banner } from '../Banner/Banner';
+import { ProjectsFilter } from '../ProjectsFilter';
 
 export const ProjectsView = () => {
+  const [seasonFilter, setSeasonFilter] = useState(0);
+
   return (
     <>
       <Banner />
-      <div className='container'>
-        <div className='my-[60px]'></div>
-        <ProjectsSection seasonFilter={0} />
+      <div className='container my-14'>
+        <ProjectsFilter
+          seasonFilter={seasonFilter}
+          setSeasonFilter={setSeasonFilter}
+        />
+        <div className=''>
+          <ProjectsSection seasonFilter={seasonFilter} />
+        </div>
       </div>
     </>
   );
