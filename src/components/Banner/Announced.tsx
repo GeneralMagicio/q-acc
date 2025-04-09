978d1bd0511788c70dbc022eb57c98188dcede50
Cherik <Pourcheriki@gmail.com>
2025-04-09 11:18:53 +0330

    add matching pool section

--- src/components/Banner/Announced.tsx
+++ src/components/Banner/Announced.tsx
@@ -1,6 +1,7 @@
 import { Inter, Nunito_Sans } from 'next/font/google';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { IconArrowRight } from '../Icons/IconArrowRight';
 import { useFetchActiveRoundDetails } from '@/hooks/useFetchActiveRoundDetails';
 import { calculateRemainingTime } from '@/helpers/date';
@@ -52,31 +53,55 @@ export const Announced = () => {
   );
 
   return (
-    <div className='flex text-peach items-center px-2 py-4 gap-6 flex-wrap justify-center relative z-10'>
-      <div className={`${inter.className} text-white`}>
-        q/acc round {isStarted ? `ends` : `starts`} in
-      </div>
-      <div className='flex flex-col items-center gap-2'>
-        <div className='text-3xl tracking-widest text-center'>
-          {remainingTime}
+    <>
+      <div className='flex text-peach items-center px-2 py-4 gap-6 flex-wrap justify-center relative z-10'>
+        <div className={`${inter.className} text-white`}>
+          q/acc round {isStarted ? `ends` : `starts`} in
         </div>
-        <div className={`${nunito.className} text-sm`}>
-          On{' '}
-          {targetDate.toLocaleString('en-US', {
-            month: 'long',
-            day: '2-digit',
-            year: 'numeric',
-          })}
+        <div className='flex flex-col items-center gap-2'>
+          <div className='text-3xl tracking-widest text-center'>
+            {remainingTime}
+          </div>
+          <div className={`${nunito.className} text-sm`}>
+            On{' '}
+            {targetDate.toLocaleString('en-US', {
+              month: 'long',
+              day: '2-digit',
+              year: 'numeric',
+            })}
+          </div>
         </div>
+        <Link href={Routes.Projects}>
+          <button
+            className={`flex items-center py-3 px-6 rounded-xl bg-gray-950 shadow-banner-button font-semibold tracking-wide ${inter.className}`}
+          >
+            <span>Get Started</span>
+            <IconArrowRight size={24} />
+          </button>
+        </Link>
       </div>
-      <Link href={Routes.Projects}>
-        <button
-          className={`flex items-center py-3 px-6 rounded-xl bg-gray-950 shadow-banner-button font-semibold tracking-wide ${inter.className}`}
-        >
-          <span>Get Started</span>
-          <IconArrowRight size={24} />
-        </button>
-      </Link>
-    </div>
+      {isStarted && (
+        <div className='flex justify-center items-center text-white bg-gray-800 w-full relative z-10 py-2 gap-20'>
+          <Image
+            src='/images/home/matching-pool.svg'
+            alt='Matching Pool image'
+            width={234}
+            height={100}
+            className='object-contain'
+          />
+          <div>
+            <div className={`${inter.className} mb-1`}>Matching Pool</div>
+            <div className='text-tusker-grotesk text-5xl'>$ 250,000</div>
+          </div>
+          <Image
+            src='/images/home/matching-pool.svg'
+            alt='Matching Pool image'
+            width={234}
+            height={100}
+            className='object-contain scale-x-[-1]'
+          />
+        </div>
+      )}
+    </>
   );
 };
