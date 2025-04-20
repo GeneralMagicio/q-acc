7425ef58c97d9eddb7f5a4b0a90a78e8c728edff
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2025-04-19 21:03:03 -0400

    made copy changes

--- src/components/DonatePage/DonateSuccessPage.tsx
+++ src/components/DonatePage/DonateSuccessPage.tsx
@@ -257,13 +257,7 @@ const DonateSuccessPage: FC<IDonateSuccessPage> = ({
 
                 <div className='border border-gray-300 bg-white rounded-lg font-redHatText text-gray-800 font-semibold leading-6 p-4'>
                   <div className='flex justify-between items-center '>
-                    <div>
-                      <span className=' '>q/acc points you've earned</span>
-                    </div>
-                    <div className='flex gap-2 items-center'>
-                      <span>
-                        {pointsEarned ? pointsEarned : '⏳ Calculating...'}
-                      </span>
+                    <div className='flex gap-1 items-center'>
                       <svg
                         xmlns='http://www.w3.org/2000/svg'
                         width='32'
@@ -277,6 +271,13 @@ const DonateSuccessPage: FC<IDonateSuccessPage> = ({
                           fill='white'
                         />
                       </svg>
+                      <span className=' '>You've earned q/acc points</span>
+                    </div>
+                    <div className='flex gap-2 items-center'>
+                      <span className='font-semibold text-giv-500 '>
+                        {/* {pointsEarned ? pointsEarned : '⏳ Calculating...'} */}
+                        <Link href={'/leaderboard'}>Check leaderboard</Link>
+                      </span>
                     </div>
                   </div>
                   {pointsEarned === null &&
