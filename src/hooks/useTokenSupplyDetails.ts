69832543ba801a24110dfa50003b325032740f1f
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2025-04-23 07:56:29 -0400

    changed token supply in dashboard

--- src/hooks/useTokenSupplyDetails.ts
+++ src/hooks/useTokenSupplyDetails.ts
@@ -3,7 +3,7 @@ import { getTokenSupplyDetails } from '@/services/tokenPrice.service';
 
 export const useTokenSupplyDetails = (contract_address: string) => {
   return useQuery({
-    queryKey: ['token-supply-details'],
+    queryKey: ['token-supply-details', contract_address],
     queryFn: async () => {
       return await getTokenSupplyDetails(contract_address);
     },
