a11f80ad0853be20b7d8d936117fca3eaabd10e6
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2025-04-28 10:52:02 -0400

    fix leaderboard

--- src/components/LeaderBoard/UserInfo.tsx
+++ src/components/LeaderBoard/UserInfo.tsx
@@ -17,15 +17,23 @@ export const UserInfo = () => {
   const userInfo = leaderboardInfo?.users?.find(cuser => cuser.id === user?.id);
 
   return (
-    <div className='p-4 border-2 border-[#5326EC] bg-gray-100 rounded-lg grid  grid-cols-[30px_120px_80px_80px] md:grid-cols-[50px_1fr_150px_120px] gap-4 font-redHatText'>
+    <div className='p-4 border-2 border-[#5326EC] bg-gray-100 rounded-lg grid grid-cols-4  sm:grid-cols-[50px_1fr_150px_120px] gap-4 font-redHatText'>
       <div># {userInfo?.rank}</div>
-      <div className='text-lg font-semibold'>
-        {shortenAddress(address)} [You]
+      <div className='col-span-2 sm:col-span-1 text-sm  sm:text-base font-semibold flex flex-col'>
+        <span>
+          {' '}
+          {shortenAddress(address)}{' '}
+          <span className='text-xs sm:text-base'>[You]</span>
+        </span>
+        <span className='sm:hidden text-xs font-redHatText text-[#999] font-medium '>
+          {' '}
+          Projects funded : {userInfo?.projectsFundedCount}
+        </span>
       </div>
-      <div className='text-lg font-semibold'>
+      <div className='text-lg font-semibold sm:text-left text-center'>
         {roundPoints(user?.qaccPoints).toLocaleString('en-US')}
       </div>
-      <div className='text-lg font-semibold'>
+      <div className='text-lg font-semibold hidden sm:block'>
         {userInfo?.projectsFundedCount}
       </div>
     </div>
